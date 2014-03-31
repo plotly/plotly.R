e77a8487a531f5891623e7c597986853f02317ce
Toby Dylan Hocking <tdhock5@gmail.com>
2014-03-31 18:25:02 -0400

    test for legend entry order

--- inst/tests/test-ggplot-legend.R
+++ inst/tests/test-ggplot-legend.R
@@ -7,3 +7,21 @@ test_that("legend can be hidden", {
   info <- gg2list(ggiris)
   expect_identical(info$kwargs$layout$showlegend, FALSE)
 })
+
+test_that("legend entries appear in the correct order", {
+  ggiris <- ggplot(iris)+
+    geom_point(aes(Petal.Width, Sepal.Width, color=Species))
+  getnames <- function(L){
+    traces <- L[names(L)==""]
+    expect_equal(length(traces), 3)
+    as.character(sapply(traces, "[[", "name"))
+  }
+  info <- gg2list(ggiris)
+  ## Default is the same as factor levels.
+  expect_identical(getnames(info), levels(iris$Species))
+  ## Custom breaks should be respected.
+  breaks <- c("versicolor", "setosa", "virginica")
+  ggbreaks <- ggiris+scale_color_discrete(breaks=breaks)
+  info.breaks <- gg2list(ggbreaks)
+  expect_identical(getnames(info.breaks), breaks)
+})
