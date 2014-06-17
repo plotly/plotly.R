77018e73ef0a52e27a9ded01493eb86910d63523
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2014-06-17 11:55:38 -0400

    Do not force default geom_bar into histogram (yet)

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -277,7 +277,6 @@ gg2list <- function(p){
     stat_type <- strsplit(stat_type, ": ")[[1]]
     if (grepl("bin", stat_type)) {
       geom_type <- "histogram"
-      p$layers[[layer.i]]$geom$objname <- "histogram"
       warning("You may want to use geom_histogram.")
     }
     pos <- capture.output(p$layers[[layer.i]]$position)
