48d8dd937b923887b72712ef0b284031f858a56e
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2014-07-31 15:48:39 -0400

    Wrap function not to pollute namespace

--- R/build_function.R
+++ R/build_function.R
@@ -8,22 +8,24 @@
 #' @return List with (data, panel, plot, prestats.data) where prestats.data
 #' is the data as it is prior to calculate_stats() call
 #' @keywords internal
-
-# Get body of the original function, in list form
-ggplot_build2 <- ggplot2::ggplot_build
-g_b <- as.list(body(ggplot_build2))
-
-# Find line where we want to insert new code
-line_after <- quote(data <- calculate_stats(panel, data, layers))
-idx <- vapply(g_b, identical, line_after, FUN.VALUE=TRUE)
-idx <- which(idx)
-
-# Insert new code before that line
-new_line <- quote(prestats.data <- data)
-return_value <- quote(list(data=data, panel=panel, plot=plot,
-                           prestats.data=prestats.data))
-g_b2 <- c(g_b[seq(1, idx-1)], list(new_line), g_b[seq(idx, length(g_b)-1)],
-          return_value)
-
-# Assign the modified body back into the function
-body(ggplot_build2) <- as.call(g_b2)
+ggplot_build2 <- function(plot) {
+  # Get body of the original function, in list form
+  ggplot_build2 <- ggplot2::ggplot_build
+  g_b <- as.list(body(ggplot_build2))
+  
+  # Find line where we want to insert new code
+  line_after <- quote(data <- calculate_stats(panel, data, layers))
+  idx <- vapply(g_b, identical, line_after, FUN.VALUE=TRUE)
+  idx <- which(idx)
+  
+  # Insert new code before that line
+  new_line <- quote(prestats.data <- data)
+  return_value <- quote(list(data=data, panel=panel, plot=plot,
+                             prestats.data=prestats.data))
+  g_b2 <- c(g_b[seq(1, idx-1)], list(new_line), g_b[seq(idx, length(g_b)-1)],
+            return_value)
+  
+  # Assign the modified body back into the function
+  body(ggplot_build2) <- as.call(g_b2)
+  ggplot_build2(plot)
+}
