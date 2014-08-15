e7322134ae267e4c1d0ed62f5da31e2100e91e10
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2014-08-15 15:48:21 -0400

    Implement conversion for geom_tile

---
+++ tests/testthat/test-ggplot-heatmap.R
@@ -0,0 +1,18 @@
+context("Heatmap")
+
+wdays <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+dtimes <- c("Morning", "Afternoon", "Evening")
+workweek <- matrix(c(1, 20, 30, 20, 1, 60, 30, 60, 1, 50, 80, -10, 1, 30, 20),
+             nrow=5, ncol=3, byrow=TRUE,
+             dimnames=list(wdays, dtimes))
+ww <- reshape2::melt(workweek)
+# Plot a heatmap using geom_tile
+hm <- ggplot(ww) + geom_tile(aes(x=Var1, y=Var2, fill=value))
+
+test_that("geom_tile is translated to type=heatmap", {
+  L <- gg2list(hm)
+  expect_equal(length(L), 2)
+  expect_identical(L[[1]]$type, "heatmap")
+  expect_identical(as.character(L[[1]]$x), wdays)
+  expect_identical(as.character(L[[1]]$y), dtimes)
+})
