0dbec50ed348504da5777b7c136b62a5f2abec2e
xsaintmleux <xavier@plot.ly>
2014-08-15 17:52:31 -0400

    cosmetic mods

--- tests/testthat/test-ggplot-boxplot.R
+++ tests/testthat/test-ggplot-boxplot.R
@@ -5,11 +5,11 @@ test_that("geom_boxplot gives a boxplot", {
   
   L <- gg2list(gg)
 
-  ## right nb. traces
+  # right nb. traces
   expect_equal(length(L), 4)
-  ## right type for 1st trace
+  # right type for 1st trace
   expect_identical(L[[1]]$type, "box")
-  ## right data for 1st trace
+  # right data for 1st trace
   expect_identical(sort(L[[1]]$y),
                    sort(mtcars$mpg[mtcars$cyl == 4]))
 })
