f7fb70135f302daae24f2ef6b2538000d3d8bfc0
Pedro Despouy <pedrodespouy@gmail.com>
2014-08-26 15:36:00 -0400

    small changes in style

--- tests/testthat/test-ggplot-area.R
+++ tests/testthat/test-ggplot-area.R
@@ -1,13 +1,12 @@
 context("Area")
 
-huron <- data.frame(year = 1875:1972, level = as.vector(LakeHuron))
-library(plyr) # to access round_any
-huron$decade <- round_any(huron$year, 10, floor)
+huron <- data.frame(year=1875:1972, level=as.vector(LakeHuron))
+huron$decade <- plyr::round_any(huron$year, 10, floor)
 
 ar <- ggplot(huron) + geom_area(aes(x=year, y=level))
 L <- gg2list(ar)
 
-test_that("geom_area is translated to type=area", {
+test_that("sanity check for geom_area", {
   expect_equal(length(L), 2)
   expect_identical(L[[1]]$type, "scatter")
   expect_equal(L[[1]]$x, c(huron$year[1], huron$year, tail(huron$year, n=1)))
