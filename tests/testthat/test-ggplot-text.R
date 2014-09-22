cc90c0e041e3c3daf607863009bc7101b58d5060
Pedro Despouy <pedrodespouy@gmail.com>
2014-09-22 17:32:22 -0400

    add spaces and clean redundancies

--- tests/testthat/test-ggplot-text.R
+++ tests/testthat/test-ggplot-text.R
@@ -1,21 +1,18 @@
 context("text")
 
-test_that("label is translated correctly",{
-  gg <- ggplot(mtcars, aes(x=wt, y=mpg, label=rownames(mtcars))) + geom_text()
-  info <- gg2list(gg)
+gg <- ggplot(mtcars, aes(x=wt, y=mpg, label=rownames(mtcars))) + 
+  geom_text(size=18)
+info <- gg2list(gg)
+
+test_that("label is translated correctly", {
   expect_identical(as.character(info[[1]]$text), rownames(mtcars))
 })
 
-test_that("position is translated correctly",{
-  gg <- ggplot(mtcars, aes(x=wt, y=mpg, label=rownames(mtcars))) + geom_text()
-  info <- gg2list(gg)
+test_that("position is translated correctly", {
   expect_identical(info[[1]]$x, mtcars$wt)
   expect_identical(info[[1]]$y, mtcars$mpg)
 })
 
-test_that("textsize is translated correctly",{
-  gg <- ggplot(mtcars, aes(x=wt, y=mpg, label=rownames(mtcars))) + 
-    geom_text(size=18)
-  info <- gg2list(gg)
+test_that("textsize is translated correctly", {
   expect_identical(info[[1]]$textfont$size, 18)
 })
