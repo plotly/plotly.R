3c804934d55cc757415515effd39a38c84acfb75
Marianne Corvellec <marianne.corvellec@ens-lyon.org>
2014-10-10 17:30:50 -0400

    Include Pedro's commit ded8766b and run our new test suite

--- tests/testthat/test-ggplot-theme.R
+++ tests/testthat/test-ggplot-theme.R
@@ -38,6 +38,22 @@ test_that("dotted/dashed grid translated as line with alpha=0.1",{
     save_outputs(ggiris, "theme-dashed-grid-lines")
 })
 
+countrypop <- data.frame(country=c("Paraguay", "Peru", "Philippines"),
+                         population=c(7, 31, 101),
+                         edu=c(4.2, 1.75, 1.33),
+                         illn=c(0.38, 1.67, 0.43))
+gg <- ggplot(countrypop) +
+  geom_point(aes(edu, illn, colour=country, size=population))
+
+test_that("marker default shape is a circle", {
+  info <- gg2list(gg)
+  for (i in c(1:3)) {
+    expect_identical(info[[i]]$marker$symbol, "circle")
+  }
+  
+  save_outputs(gg, "theme-marker-default")
+})
+
 test_that("plot panel border is translated correctly", {
   ggiris <- iris.base + theme_grey() # has no panel.border
   info <- gg2list(ggiris)
