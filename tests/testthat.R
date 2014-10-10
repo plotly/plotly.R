e21ac098af6d93ad24bd024ee0db83fcd8ed4862
Chris P <christopher.parmer@gmail.com>
2014-10-10 14:19:26 -0400

    separate travis tests from manual suite tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,19 +1,3 @@
 library(testthat)
-
-save_outputs <- function(gg, name, ignore_ggplot=FALSE) {
-  py <- plotly("TestBot", "r1neazxo9w")
-  u <- py$ggplotly(gg, kwargs=list(filename=paste0("ggplot2/", name),
-                                   fileopt="overwrite", auto_open=FALSE))
-  plotlyUrl <- u$response$url
-  writeLines(plotlyUrl, paste0("test-ggplot-", name, ".url"))
-  pngdata <- getURLContent(paste0(u$response$url, ".png"))
-  writeBin(as.raw(pngdata), paste0("test-ggplot-", name, ".png"))
-  if (!ignore_ggplot) {
-    ggsave(paste0("test-ggplot-", name, "-orig.png"), plot=gg, w=7, h=5)
-  }
-
-  # save the json
-  writeLines(getURL(paste0(plotlyUrl, ".json")), paste0("test-ggplot-", name, ".json"))
-}
-
+save_outputs <- function(gg, name, ignore_ggplot=FALSE) {}
 test_check("plotly")
