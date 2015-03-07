e97ee3ab1c0624d09aefc12f583813d2aa0939e0
Toby Dylan Hocking <tdhock5@gmail.com>
2015-03-06 19:59:12 -0500

    rect test fails

---
+++ tests/testthat/test-ggplot-rect.R
@@ -0,0 +1,28 @@
+context("geom_rect")
+
+expect_traces <- function(gg, n.traces, name){
+  stopifnot(is.ggplot(gg))
+  stopifnot(is.numeric(n.traces))
+  save_outputs(gg, paste0("rects-", name))
+  L <- gg2list(gg)
+  is.trace <- names(L) == ""
+  all.traces <- L[is.trace]
+  no.data <- sapply(all.traces, function(tr) {
+    is.null(tr[["x"]]) && is.null(tr[["y"]])
+  })
+  has.data <- all.traces[!no.data]
+  expect_equal(length(has.data), n.traces)
+  list(traces=has.data, kwargs=L$kwargs)
+}
+
+df <- data.frame(
+  x = sample(10, 20, replace = TRUE),
+  y = sample(10, 20, replace = TRUE)
+)
+
+gg <- ggplot(df, aes(xmin = x, xmax = x + 1, ymin = y, ymax = y + 2)) +
+  geom_rect()
+
+test_that('geom_rect becomes traces with mode "lines" with fill "tozerox"', {
+  expect_traces(gg, 1, "black")
+})
