9b5bbf306ada7d25d2f7d778f2cb11f69fc437dd
cpsievert <cpsievert1@gmail.com>
2015-03-10 12:05:08 -0500

    save_outputs of smooth tests

--- tests/testthat/test-ggplot-smooth.R
+++ tests/testthat/test-ggplot-smooth.R
@@ -5,6 +5,7 @@ p <- ggplot(mtcars, aes(mpg, wt)) + geom_point() + geom_smooth()
 test_that("geom_point() + geom_smooth() produces 3 traces", {
   info <- gg2list(p)
   expect_true(sum(names(info) == "") == 3)
+  save_outputs(p, "smooth")
 })
 
 p2 <- ggplot(mtcars, aes(mpg, wt)) + geom_point() + geom_smooth(se = FALSE)
@@ -12,5 +13,6 @@ p2 <- ggplot(mtcars, aes(mpg, wt)) + geom_point() + geom_smooth(se = FALSE)
 test_that("geom_point() + geom_smooth(se = FALSE) produces 2 traces", {
   info2 <- gg2list(p2)
   expect_true(sum(names(info2) == "") == 2)
+  save_outputs(p2, "smooth-se-false")
 })
 
