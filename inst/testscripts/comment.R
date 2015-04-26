048df203dbbb4bd446eb61a769dc7595299bac0a
cpsievert <cpsievert1@gmail.com>
2015-04-25 19:05:36 -0500

    Try issues, not pulls

--- inst/testscripts/comment.R
+++ inst/testscripts/comment.R
@@ -5,7 +5,7 @@ a <- commandArgs(TRUE)
 # https://developer.github.com/v3/pulls/comments/#create-a-comment
 # note that gistr is a good reference for talking to the github API via httr
 # https://github.com/ropensci/gistr/blob/master/R/zzz.R
-url <- sprintf('https://api.github.com/repos/ropensci/plotly/pulls/%s/comments', a[1])
+url <- sprintf('https://api.github.com/repos/ropensci/plotly/issues/%s/comments', a[1])
 tables <- sprintf("http://ropensci.github.io/plotly-test-table/tables/%s/index.html", a[2])
 header <- httr::add_headers(`User-Agent` = "plotly", `Accept` = 'application/vnd.github.v3+json')
 msg <- sprintf("New test table created at \n '%s'", tables)
