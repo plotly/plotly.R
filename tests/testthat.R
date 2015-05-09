cb6e10419def08c76778ba946b0a81bf57c26a39
cpsievert <cpsievert1@gmail.com>
2015-05-09 00:43:32 -0500

    if you use a package in tests, you _must_ put in DESCRIPTION now

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,7 @@
 library("testthat")
 library("plotly")
-library("devtools")
 # crendentials for the test bot
 Sys.setenv(`plotly-username` = "TestBot")
 Sys.setenv(`plotly-apikey` = "r1neazxo9w")
 source(system.file("testscripts/save_outputs.R", package = "plotly"))
-# avoid weird errors if this function is called via testhat::check()
-# https://github.com/hadley/testthat/issues/144
-Sys.setenv("R_TESTS" = "")
 test_check("plotly")
