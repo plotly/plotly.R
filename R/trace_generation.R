dfb079ae85e03f0f8f4b9b7b21efe57089ae5bb0
cpsievert <cpsievert1@gmail.com>
2015-05-20 22:40:51 -0500

    get test working; jitter shouldn't be a basic geom

--- R/trace_generation.R
+++ R/trace_generation.R
@@ -384,6 +384,14 @@ toBasic <- list(
     g$params$yend <- max(g$prestats.data$globymax)
     g
   },
+  jitter=function(g) {
+    if ("size" %in% names(g$data)) {
+      g$params$sizemin <- min(g$prestats.data$globsizemin)
+      g$params$sizemax <- max(g$prestats.data$globsizemax)
+    }
+    g$geom <- "point"
+    g
+  },
   point=function(g) {
     if ("size" %in% names(g$data)) {
       g$params$sizemin <- min(g$prestats.data$globsizemin)
@@ -555,38 +563,6 @@ geom2trace <- list(
       L$visible <- FALSE
     }
     L
-  },
-  jitter=function(data, params){
-    L <- list(x = data$x,
-              y = data$y,
-              name = params$name,
-              text = data$text,
-              type = "scatter",
-              mode = "markers",
-              marker = paramORdefault(params, aes2marker, marker.defaults))
-    if ("size" %in% names(data)) {
-      L$text <- paste("size:", data$size)
-      L$marker$sizeref <- default.marker.sizeref
-      # Make sure sizes are passed as a list even when there is only one element.
-      s <- data$size
-      marker.size <- 5 * (s - params$sizemin) / 
-        (params$sizemax - params$sizemin) + 0.25
-      marker.size <- marker.size * marker.size.mult
-      L$marker$size <- ifelse(length(s) > 1, marker.size, list(marker.size))
-      L$marker$line$width <- 0
-    }
-    if (!is.null(params$shape) && params$shape %in% c(21:25)) {
-      L$marker$color <- ifelse(!is.null(params$fill), 
-                               toRGB(params$fill), "rgba(0,0,0,0)")
-      if (!is.null(params$colour)) {
-        L$marker$line$color <- toRGB(params$colour) 
-      }
-      L$marker$line$width <- 1
-    }
-    if (!is.null(params$shape) && params$shape %in% c(32)) {
-      L$visible <- FALSE
-    }
-    return(L)
   },
   text=function(data, params){
     L <- list(x=data$x,
--- tests/testthat/test-ggplot-jitter.R
+++ tests/testthat/test-ggplot-jitter.R
@@ -1,11 +1,10 @@
 context("geom_jitter")
 
 # Expect trace function
-expect_traces <- function(gg, n_traces, name, seed) {
+expect_traces <- function(gg, n_traces, name) {
   stopifnot(is.ggplot(gg))
   stopifnot(is.numeric(n_traces))
-  save_outputs(gg, paste0("coord_fixed-", name))
-  set.seed(seed)
+  save_outputs(gg, paste0("jitter-", name))
   L <- gg2list(gg)
   all_traces <- L$data
   no_data <- sapply(all_traces, function(tr) {
@@ -16,27 +15,13 @@ expect_traces <- function(gg, n_traces, name, seed) {
   list(traces = has_data, layout = L$layout)
 }
 
-#head(L$data[[1]]$x)
-
-# get data from mpg dataset
-p <- ggplot(mpg, aes(displ, hwy))
-
-# Test 1
-# set up the data
 set.seed(1001)
-p1 <- ggplot() + geom_jitter(data = mpg, aes(displ, hwy), width = 1)
-head(ggplot_build2(p1)$data[[1]]$x)
-# test
-test_that("geom_jitter is working", {
-  info <- expect_traces(p1, 1, "geom_jitter", 1001)
-  tr <- info$traces[[1]]
-  la <- info$layout
-  expect_identical(tr$type, "scatter")
-  set.seed(1001)
-  built <- ggplot_build2(p1)
-  print(head(tr$x)) # from gg2list
-  print(head(built$data[[1]]$x)) # from ggplot_build2
-  expect_identical(tr$x, built$data[[1]]$x)
-  expect_identical(tr$y, built$data[[1]]$y)
-})
+p <- ggplot(mpg, aes(cyl, hwy)) + geom_jitter()
 
+test_that("geom_jitter is working", {
+  info <- expect_traces(p1, 1, "basic")
+  tr <- info$traces[[1]]
+  expect_identical(tr$type, "scatter")
+  # default jitter is 40% of the resolution of the data.
+  expect_true(all(0 < abs(mpg$cyl - tr$x) < 0.4))
+})
