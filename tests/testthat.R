ed6a586721bc2eb9bc0d3fa5d600b6ed442d971f
cpsievert <cpsievert1@gmail.com>
2015-06-16 13:40:23 -0500

    Simply save_output logic

--- tests/testthat.R
+++ tests/testthat.R
@@ -17,45 +17,46 @@ hash <- if (src == "local") {
 save_outputs <- function(gg, name) {
   # only render/save pngs if this is a Travis pull request
   # (see build-comment-push.R for better explanation of this logic)
+  message("Running test: ", name)
   tpr <- Sys.getenv("TRAVIS_PULL_REQUEST")
   if (tpr != "false" && tpr != "") {
-    table_dir <-
-      file.path(Sys.getenv("TRAVIS_BUILD_DIR"), "..", "plotly-test-table")
+    table_dir <- file.path(Sys.getenv("TRAVIS_BUILD_DIR"), 
+                           "..", "plotly-test-table")
     plotly_dir <- file.path(table_dir, "R", hash)
-    gg_dir <- file.path(table_dir, "R", "ggplot2")
     if (!dir.exists(plotly_dir)) dir.create(plotly_dir, recursive = TRUE)
-    if (!dir.exists(gg_dir)) dir.create(gg_dir, recursive = TRUE)
-
-    # If we don't have pngs for this version (of ggplot2), generate them;
-    # otherwise, generate plotly pngs
-    # NOTE: we can't save both plotly & ggplot2 at once since R CMD check
-    # suppresses output and travis has 10 minute time limit
-    # https://github.com/travis-ci/travis-ci/issues/3849
-    ggversion <- as.character(packageVersion("ggplot2"))
-    if (!ggversion %in% dir(gg_dir)) {
-      gglife <- file.path(gg_dir, ggversion)
-      dir.create(gglife, recursive = TRUE)
-      e <- try(gg, silent = TRUE)
-      png(filename = file.path(gglife, paste0(name, ".png")))
-      if (inherits(e, "try-error")) {
-        plot(1, type="n")
-        text(1, "ggplot2 error")
-      } else gg
-      dev.off()
-    } else  {
-      # TODO: could speed things up by avoiding two calls to gg2list()
-      # (this will require tweaking expect_traces())
-      p <- print(ggplotly(gg))
-      png_url <- paste0(p[["url"]], ".png")
-      resp <- httr::GET(png_url)
-      # print the response if it wasn't successful
-      if (httr::warn_for_status(resp)) resp
-      # write png version of plotly figure to disk
-      writeBin(httr::content(resp, as = "raw"),
-               file.path(plotly_dir, paste0(name, ".png")))
-    }
-  }
+    p <- print(ggplotly(gg))
+    png_url <- paste0(p[["url"]], ".png")
+    resp <- httr::GET(png_url)
+    # print the response if it wasn't successful
+    if (httr::warn_for_status(resp)) resp
+    # write png version of plotly figure to disk
+    writeBin(httr::content(resp, as = "raw"),
+             file.path(plotly_dir, paste0(name, ".png")))
+  } 
   invisible(NULL)
+  
+  #gg_dir <- file.path(table_dir, "R", "ggplot2")
+  #if (!dir.exists(gg_dir)) dir.create(gg_dir, recursive = TRUE)
+  
+  # If we don't have pngs for this version (of ggplot2), generate them;
+  # otherwise, generate plotly pngs
+  # NOTE: we can't save both plotly & ggplot2 at once since R CMD check
+  # suppresses output and travis has 10 minute time limit
+  # https://github.com/travis-ci/travis-ci/issues/3849
+  #     ggversion <- as.character(packageVersion("ggplot2"))
+  #     if (!ggversion %in% dir(gg_dir)) {
+  #       gglife <- file.path(gg_dir, ggversion)
+  #       dir.create(gglife, recursive = TRUE)
+  #       e <- try(gg, silent = TRUE)
+  #       png(filename = file.path(gglife, paste0(name, ".png")))
+  #       if (inherits(e, "try-error")) {
+  #         plot(1, type="n")
+  #         text(1, "ggplot2 error")
+  #       } else gg
+  #       dev.off()
+  #     } else  {
+  #     }
+  
 }
 
 test_check("plotly")
