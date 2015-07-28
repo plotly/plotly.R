bb4b53113ad8ffd54b08253c961205b5a6685c7f
cpsievert <cpsievert1@gmail.com>
2015-07-28 18:08:32 -0400

    make sure title is character string before using as a title

--- R/plotly_POST.R
+++ R/plotly_POST.R
@@ -35,13 +35,18 @@ plotly_POST <- function(x) {
   
   # filename & fileopt are keyword arguments required by the API
   # (note they can also be specified by the user)
-  if (!is.null(x$url) || !is.null(kwargs$filename)) kwargs$fileopt <- "overwrite"
-  if (is.null(kwargs$filename)) 
-    kwargs$filename <- kwargs$layout$title %||% 
-    with(kwargs$layout, paste(c(xaxis, yaxis, zaxis), collapse = "vs.")) %||%
-    "plot from api" 
-  if (is.null(kwargs$fileopt)) kwargs$fileopt <- "new"
   
+  if (!is.null(x$url) || !is.null(kwargs$filename)) kwargs$fileopt <- "overwrite"
+  if (is.null(kwargs$filename)) {
+    kwargs$filename <- 
+      as.character(kwargs$layout$title) %||% 
+      paste(
+        c(kwargs$layout$xaxis, kwargs$layout$yaxis, kwargs$layout$zaxis), 
+        collapse = "vs."
+      ) %||%
+      "plot from api" 
+  }
+  if (is.null(kwargs$fileopt)) kwargs$fileopt <- "new"
   # construct body of message to plotly server
   bod <- list(
     un = verify("username"),
