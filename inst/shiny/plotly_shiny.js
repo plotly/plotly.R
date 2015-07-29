5bd986ec0e7fa2058dbc26e8d6927c379d47e6ca
cpsievert <cpsievert1@gmail.com>
2015-07-29 14:08:29 -0400

    fix bug with plotly offline

--- inst/shiny/plotly_shiny.js
+++ inst/shiny/plotly_shiny.js
@@ -14,20 +14,32 @@ binding.renderValue = function(el, dat) {
     // for offline plots, this element is a div; otherwise, an iframe
     var $el = $(el);
     
-    if (dat[0].offline === true) {
-      // why does this add traces instead of create a new plot?
-      Plotly.newPlot($el.attr("id"), dat[0].data, dat[0].layout);
+    // use Plotly Offline?
+    var offline = dat[0].offline;
+    // data attached to this element (none is attached at startup)
+    // provides a nifty mechanism to distinguish between initiating & modifying 
+    // the element -> https://github.com/jcheng5/shiny-js-examples/blob/master/output/www/linechart-binding.js
+    var plotlyDat = $el.data("plotly");
+    
+    if (offline) {
+      
+      if (!plotlyDat) {
+        $el.data("plotly", {plotly: 'rules'});
+      }
+      // Have to give newPlot a DOM representation of the <div> 
+      // (instead of the id string)
+      var gd = document.getElementById($el.attr('id'));
+      Plotly.newPlot(gd, dat[0].data, dat[0].layout);
+      
     } else {
       // see https://github.com/plotly/postMessage-API#newPlot
       var msg = {task: 'newPlot', data: dat[0].data, layout: dat[0].layout};
       var plot = $el[0].contentWindow;
       // If we haven't attached any data to this binding, ping plotly to 
       // make sure it's ready to receive message(s). This will prevent posting
-      // n >= 1 messages everytime shiny inputs change
-      // Combines ideas from:
-      // https://github.com/plotly/postMessage-API#ping
-      // https://github.com/jcheng5/shiny-js-examples/blob/master/output/www/linechart-binding.js
-      if (!$el.data("plotly")) {
+      // n >= 1 messages everytime shiny inputs change.
+      if (!plotlyDat) {
+        // see https://github.com/plotly/postMessage-API#ping
         var pinger = setInterval(function() {
           plot.postMessage({task: 'ping'}, 'https://plot.ly');
         }, 500);
@@ -42,10 +54,8 @@ binding.renderValue = function(el, dat) {
         // now attach some arbitrary data
         $el.data("plotly", {plotly: 'rules'});
       }
-      
       plot.postMessage(msg, 'https://plot.ly');
     }
-    
 };
 
 Shiny.outputBindings.register(binding, "plotlyEmbed");
