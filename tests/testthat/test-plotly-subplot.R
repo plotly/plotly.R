feb7a58b7000a8bfbced6f52841e27c881645f06
cpsievert <cpsievert1@gmail.com>
2015-08-25 10:26:57 -0500

    key field tracks subplot counter, not plot; add a test for subplot with group/[x-y]axis specification

--- tests/testthat/test-plotly-subplot.R
+++ tests/testthat/test-plotly-subplot.R
@@ -27,6 +27,20 @@ test_that("nrows argument works", {
   expect_true(doms$yaxis[1] >= doms$yaxis2[2])
 })
 
+test_that("group + [x/y]axis works", {
+  iris$id <- as.integer(iris$Species)
+  p <- plot_ly(iris, x = Petal.Length, y = Petal.Width, group = Species,
+               xaxis = paste0("x", id), mode = "markers")
+  s <- expect_traces(subplot(p, margin = 0.05), 3, "group")
+  doms <- lapply(s$layout, "[[", "domain")
+  # make sure y domain is [0, 1] on every axis
+  ydom <- doms[grepl("^y", names(doms))]
+  expect_equal(sort(unique(unlist(ydom))), c(0, 1))
+  xdom <- doms[grepl("^x", names(doms))]
+  expect_true(all(1/3 > xdom[[1]] & xdom[[1]] >= 0))
+  expect_true(all(2/3 > xdom[[2]] & xdom[[2]] > 1/3))
+  expect_true(all(1 >= xdom[[3]] & xdom[[3]] > 2/3))
+})
 
 
 
