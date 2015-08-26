5a400383289ea137af26720da36c9b013496f771
chriddyp <chris@plot.ly>
2015-08-26 12:55:48 -0400

    file.info returns file size in bytes
    `useBytes` - logical: For readChar, should nchars be regarded as a
    number of bytes not characters in a multi-byte locale?
    
    This *may* fix an issue
    ```
    In readChar(off, file.info(off)$size) :
    can only read in bytes in a non-UTF-8 MBCS locale
    ```
    from a user with a Japanese environment

--- R/print.R
+++ R/print.R
@@ -41,7 +41,7 @@ knit_print.plotly <- function(x, options, ...) {
 #' @export
 print.offline <- function(x, ...) {
   off <- offline_bundle(jq = TRUE)
-  plotlyjs <- readChar(off, file.info(off)$size)
+  plotlyjs <- readChar(off, file.info(off)$size, useBytes=TRUE)
   html <- sprintf(
     '<!DOCTYPE html><html lang="en">
      <head>
@@ -80,7 +80,7 @@ knit_print.offline <- function(x, options, ...) {
   # if this is the first plot, place bundle just before the plot
   if (length(knitr::knit_meta(class = "plotly", clean = FALSE)) == 0) {
     off <- offline_bundle(jq = TRUE)
-    b <- readChar(off, file.info(off)$size)
+    b <- readChar(off, file.info(off)$size, useBytes=TRUE)
     p <- paste0(
       sprintf('<script type="text/javascript">%s</script>', b),
       p
