df79d997aa072a74a1681d9b8d3ac1dd5298d997
cpsievert <cpsievert1@gmail.com>
2015-10-05 12:22:43 +1100

    have to use plotly_POST() to post figures now

--- tests/testthat/test-plotly-filename.R
+++ tests/testthat/test-plotly-filename.R
@@ -1,6 +1,6 @@
 context("Filename")
 
 test_that("filepath with directories is returned as passed", {
-  p <- print(plot_ly(mtcars, x = wt, y = vs, filename = "directory/awesome"))
-  expect_match(p$filename, "directory/awesome")
+  p <- plot_ly(mtcars, x = wt, y = vs, filename = "directory/awesome")
+  expect_match(plotly_POST(p)$filename, "directory/awesome")
 })
