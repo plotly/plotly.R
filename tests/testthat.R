dc157a8320188767bcc27c17d9be26789e897b8e
cpsievert <cpsievert1@gmail.com>
2015-10-12 15:25:31 +1100

    simplify test reporting

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,36 +1,25 @@
 library("testthat")
 library("plotly")
 
-# find the hash of the currently installed plotly package
-pkg_info <- devtools::session_info()$packages
-src <- subset(pkg_info, package == "plotly")$source
-hash <- if (src == "local") {
-  # you could also do `git rev-parse HEAD`, but this is cleaner for Travis
-  substr(Sys.getenv("TRAVIS_COMMIT"), 1, 7)
-} else {
-  # thankfully devtools includes hash for packages installed off GitHub
-  sub("\\)", "", strsplit(src, "@")[[1]][2])
-}
-# setup directory for placing files during tests
-# (note the working directory should be /path/to/plotly/tests)
-table_dir <- normalizePath("../../plotly-test-table")
-plotly_dir <- file.path(table_dir, "R", hash)
-plotly_thumb_dir <- file.path(plotly_dir, "thumbs")
-dir.create(plotly_thumb_dir, showWarnings = FALSE, recursive = TRUE)
+# hash of the version being tested
+this_hash <- substr(Sys.getenv("TRAVIS_COMMIT"), 1, 7)
+# we may want to compare results with master (if this is a Travis pull request)
+master_hash <- substr(system("git rev-parse master", intern = T), 1, 7)
+check_tests <- grepl("^[0-9]+$", Sys.getenv("TRAVIS_PULL_REQUEST"))
 
-# in case we need save ggplot2 output
-ggversion <- as.character(packageVersion("ggplot2"))
-gg_dir <- file.path(table_dir, "R", paste0("ggplot2-", ggversion))
-gg_thumb_dir <- file.path(gg_dir, "thumbs")
-dir.create(gg_thumb_dir, showWarnings = FALSE, recursive = TRUE)
-gg_names <- sub("\\.png$", "", dir(gg_dir, pattern = "\\.png$"))
+# plotly-test-table repo hosts the diff pages & keeps track of previous versions
+table_dir <- normalizePath("../../plotly-test-table", mustWork = TRUE)
+this_dir <- file.path(table_dir, "R", this_hash)
+master_dir <- file.path(table_dir, "R", master_hash)
 
-# text file that tracks figure hashes
+# text file that tracks plot hashes
 hash_file <- file.path(table_dir, "R", "hashes.csv")
 if (!file.exists(hash_file)) {
   file.create(hash_file)
   cat("commit,test,hash\n", file = hash_file, append = TRUE)
 }
+hash_info <- utils::read.csv(hash_file)
+master_info <- hash_info[hash_info$commit %in% master_hash, ]
 
 # This function is called within testthat/test-*.R files.
 # It takes a ggplot or plotly object as input, and it returns a figure
@@ -39,56 +28,47 @@ if (!file.exists(hash_file)) {
 # it will POST figures to plotly and save pngs 
 save_outputs <- function(gg, name) {
   print(paste("Running test:", name))
-  p <- if (is.ggplot(gg)) gg2list(gg) else plotly_build(gg)
-  tpr <- Sys.getenv("TRAVIS_PULL_REQUEST")
-  # only create images if this is a Travis pull request
-  if (tpr != "false" && tpr != "") {
-    # save a hash of the R object sent to the plotly server
-    # (eventually use this to prevent redundant POSTs?!)
-    info <- paste(hash, name, digest::digest(p), sep = ",")
+  p <- plotly_build(gg)
+  if (check_tests) {
+    # save a hash of the R object
+    plot_hash <- digest::digest(p)
+    info <- paste(hash, name, plot_hash, sep = ",")
     cat(paste(info, "\n"), file = hash_file, append = TRUE)
-    filename <- file.path(plotly_dir, paste0(name, ".png"))
-    if (file.exists(filename))
-      stop(shQuote(name), " has already been used to save_outputs() in another test.")
-    # Using plotly_IMAGE() requires 1.0.8 or higher
-    if (packageVersion("plotly") < "1.0.8") 
-      stop("These tests assume you're running plotly version 1.0.8 or higher")
-    print(system.time(resp <- plotly_IMAGE(p, height = 500, width = 700, out_file = filename)))
-    # now convert png to a smaller size
-    argz <- c(filename, "-density", "36x36", "-write", 
-              file.path(plotly_thumb_dir, paste0(name, ".png")), "+delete")
-    system2("convert", argz)
-    
-    # write the JSON used to create the image to disk for diffing purposes
-    writeLines(
-      plotly:::to_JSON(p), 
-      file.path(plotly_dir, paste0(name, ".json"))
-    )
-    
-    # if missing, save the ggplot2 version
-    # do an else if to take advantage of both builds?
-    if (!name %in% gg_names) {
-      gg_file <- file.path(gg_dir, paste0(name, ".png"))
-      png(gg_file, width = 700, height = 500)
-      try(print(gg))
-      dev.off()
-      # now convert png to a smaller size
-      argz <- c(gg_file, "-density", "72x72", "-write", 
-                file.path(gg_thumb_dir, paste0(name, ".png")), "+delete")
-      system2("convert", argz)
+    # if the plot hash is different from master, build using the master branch
+    test_info <- master_info[master_info$test %in% name, ]
+    if (plot_hash != test_info$hash) {
+      pm <- run_master(gg)
+      # it could be that the hash didn't exist, so make sure they're different
+      if (plot_hash != digest::digest(pm)) {
+        if (packageVersion("plotly") < "1.0.8") stop("These tests assume you're running plotly version 1.0.8 or higher", call. = F)
+        # copy over diffing template
+        jsondiff <- dir("../inst/jsondiff", full.names = TRUE)
+        test_dir <- file.path(this_dir, name)
+        if (dir.exists(test_dir)) stop(shQuote(name), " has already been used to save_outputs() in another test.")
+        dir.create(test_dir)
+        file.copy(jsondiff, test_dir, recursive = TRUE)
+        # overwrite the default JSON
+        writeLines(
+          plotly:::to_JSON(p), 
+          file.path(test_dir, "New.json")
+        )
+        writeLines(
+          plotly:::to_JSON(pm), 
+          file.path(test_dir, "Old.json")
+        )
+      }
     }
   }
   p
 }
 
-tryWhile <- function(expr, times = 20) {
-  e <- try(expr)
-  while (inherits(e, "try-error") && times > 0) {
-    Sys.sleep(0.5)
-    times <- times - 1
-    e <- try(expr)
-  }
-  e
+# a hack to run plotly_build() using code on the HEAD of master 
+# (instead of the current version being tested)
+run_master <- function(p) {
+  saveRDS(p, "tmp.rds")
+  system("git checkout master; Rscript -e 'devtools::load_all(); saveRDS(plotly::plotly_build(p <- readRDS(\"tmp.rds\")), \"tmp.rds\")'")
+  on.exit(unlink("tmp.rds"))
+  readRDS("tmp.rds")
 }
 
 test_check("plotly")
