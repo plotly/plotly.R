10653ca42a3a573d14518aa834497358ceb12b94
cpsievert <cpsievert1@gmail.com>
2015-10-31 11:18:38 +1100

    Throw warning instead of error on package load if using an incompatible version of ggplot2

--- R/build_function.R
+++ R/build_function.R
@@ -10,23 +10,29 @@
 #' @keywords internal
 #' @export
 ggplot_build2 <- local({
-  # Get body of the original function, in list form
-  ggplot_build2 <- ggplot2::ggplot_build
-  g_b <- as.list(body(ggplot_build2))
-  
-  # Find line where we want to insert new code
-  line_after <- quote(data <- calculate_stats(panel, data, layers))
-  idx <- vapply(g_b, identical, line_after, FUN.VALUE=TRUE)
-  idx <- which(idx)
-  
-  # Insert new code before that line
-  new_line <- quote(prestats.data <- data)
-  return_value <- quote(list(data=data, panel=panel, plot=plot,
-                             prestats.data=prestats.data))
-  g_b2 <- c(g_b[seq(1, idx-1)], list(new_line), g_b[seq(idx, length(g_b)-1)],
-            return_value)
-  
-  # Assign the modified body back into the function
-  body(ggplot_build2) <- as.call(g_b2)
-  ggplot_build2
+  tryCatch({
+    # Get body of the original function, in list form
+    ggplot_build2 <- ggplot2::ggplot_build
+    g_b <- as.list(body(ggplot_build2))
+    
+    # Find line where we want to insert new code
+    line_after <- quote(data <- calculate_stats(panel, data, layers))
+    idx <- vapply(g_b, identical, line_after, FUN.VALUE=TRUE)
+    idx <- which(idx)
+    
+    if (length(idx) != 1) {
+      warning("ggplotly() is not compatible with this version of ggplot2", call. = F)
+    } else {
+      # Insert new code before that line
+      new_line <- quote(prestats.data <- data)
+      return_value <- quote(list(data=data, panel=panel, plot=plot,
+                                 prestats.data=prestats.data))
+      g_b2 <- c(g_b[seq(1, idx-1)], list(new_line), g_b[seq(idx, length(g_b)-1)],
+                return_value)
+      
+      # Assign the modified body back into the function
+      body(ggplot_build2) <- as.call(g_b2)
+    }
+    ggplot_build2
+  })
 })
