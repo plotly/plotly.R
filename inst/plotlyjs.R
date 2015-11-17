ba918c9e0111a3470dbff72a35d83be13bdda886
cpsievert <cpsievert1@gmail.com>
2015-11-17 17:27:42 +1100

    add plotlyjs LICENSE

--- inst/plotlyjs.R
+++ inst/plotlyjs.R
@@ -2,3 +2,8 @@ download.file(
   "http://cdn.plot.ly/plotly-latest.min.js",
   "inst/htmlwidgets/lib/plotlyjs/plotly-latest.min.js"
 )
+
+download.file(
+  "https://raw.githubusercontent.com/plotly/plotly.js/master/LICENSE",
+  "inst/htmlwidgets/lib/plotlyjs/LICENSE"
+)
