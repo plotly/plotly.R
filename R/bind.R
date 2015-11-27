1e35205241abc9835f74230f9c7a2609e8e91dbb
cpsievert <cpsievert1@gmail.com>
2015-11-27 16:09:17 +1100

    callback can be character string

--- R/bind.R
+++ R/bind.R
@@ -4,8 +4,9 @@
 #' 
 #' @param p a plotly visualization
 #' @param event A valid DOM event 
-#' @param callback the body of a JavaScript callback function 
-#' with the arguments \code{event} and \code{data} to be applied to the plotly instance
+#' @param callback character string containing the body of a JavaScript 
+#' callback function with the arguments \code{event} and \code{data} 
+#' to be applied to the plotly instance.
 #' @export
 #' @importFrom htmlwidgets JS
 #' @examples 
@@ -14,15 +15,20 @@
 #' # open this in a web browser, click on a point, and open your console
 #' bind(p)
 #' # access the clicked point and display alert box
-#' cb <- "var pts = data.points[0]; alert('You selected ['+pts.x+', '+pts.y+']');"
+#' cb <- c(
+#'   "var pts = data.points[0];",
+#'   "alert('You selected ['+pts.x+', '+pts.y+']');"
+#'  )
 #' bind(p, callback = cb)
 #'
 
 bind <- function(p, event = "plotly_click", callback = "console.log(event, data)") {
   p <- last_plot(p)
+  if (missing(callback)) callback <- "return null;"
+  if (length(callback) == 0) callback <- "return null;"
   p[["callback"]] <- JS(sprintf(
     "$('#%s').bind('%s', function(event, data) { %s })", 
-    p$elementId, event, callback
+    p$elementId, event, JS(callback)
   ))
   hash_plot(if (is.data.frame(p)) p else list(), p)
 }
