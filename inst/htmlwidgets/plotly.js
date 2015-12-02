23e7afabeffde552983e6e8c63996b0fb4714ac9
cpsievert <cpsievert1@gmail.com>
2015-12-02 13:07:31 +1100

    cleanup js mess

--- inst/htmlwidgets/plotly.js
+++ inst/htmlwidgets/plotly.js
@@ -21,26 +21,21 @@ HTMLWidgets.widget({
       Plotly.newPlot(el.id, x.data, x.layout);
     }
     
-    // TODO: pass in group from R!
-    var ctgrp = crosstalk.group('grp');
-    
-    console.log(x.elementId);
-    dat = x.data;
+    var ctgrp = crosstalk.group(x.set);
     
     // was a selection key specified?
     if (x.data[0].hasOwnProperty("key")) {
       // TODO: pass in event from R!
       $('#'+el.id).on('plotly_click', function(event, data) {
         
-        pts = data.points;
-        keys = data.points.map(function(pt){
+        var keys = data.points.map(function(pt){
           return pt.data.key[pt.pointNumber];
         });
+        
         ctgrp.var("tdb").set(keys);
       });
       
       ctgrp.var("tdb").on('change', function(e) {
-        console.log(e.value);
         
         var opacity = x.data.map(function(tr) {
            var o = tr.key.map(function(k) {
@@ -50,7 +45,7 @@ HTMLWidgets.widget({
            return o;
         });
        
-        op = {"opacity": opacity};
+        op = {"marker.opacity": opacity};
         
         Plotly.restyle(el, op);
       });
@@ -60,30 +55,3 @@ HTMLWidgets.widget({
   }
   
 });
-
-
-/*
-        // decrease the opacity of every point
-        var op = [];
-        for (var i = 0; i < x.data.length; i++) {
-           var d = x.data[i];
-           var n = d.key.length;
-           // TODO: smart preservation of specified opacity?
-           var o = [];
-           for (j = 0; j < n; j++) {
-             o.push(0.2);
-           }
-           op.push(o);
-        }
-        
-        // increase the opacity of clicked point(s)
-        for (var k = 0; k < data.points.length; k++) {
-          var pt = data.points[k];
-          op[pt.curveNumber][pt.pointNumber] = 1;
-        }
-        
-        var opacity = {"opacity": op};
-        
-        ctgrp.var("tdb").set(opacity);
-        */
-        
