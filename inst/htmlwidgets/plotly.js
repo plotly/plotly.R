d180812556ad03f1255913eadc2744cfc601f4bf
cpsievert <cpsievert1@gmail.com>
2015-12-08 14:01:23 +1100

    send a subset of th data.points object

--- inst/htmlwidgets/plotly.js
+++ inst/htmlwidgets/plotly.js
@@ -22,16 +22,34 @@ HTMLWidgets.widget({
     }
     
     var g = x.data[0].set;
-    console.log(g);
     var grp = crosstalk.group(g);
+    
     $('#'+el.id).on('plotly_click', function(event, data) {
+      // extract only the data we may want to access in R
+      var d = data.points.map(function(pt) {
+        var obj = {
+            curveNumber: pt.curveNumber, 
+            pointNumber: pt.pointNumber, 
+            x: pt.x,
+            y: pt.y
+        };
+        if (pt.data.hasOwnProperty("key")) {
+          if (typeof pt.pointNumber === "number") {
+            obj.key = pt.data.key[pt.pointNumber];
+          } else {
+            obj.key = pt.data.key[pt.pointNumber[0]][pt.pointNumber[1]];
+          } // TODO: can pointNumber be 3D?
+        }
+        return obj;
+      });
+      
       // tell crosstalk about click data so we can access it in R (and JS)
-      grp.var("plotly_click").set(data.points);
+      grp.var("plotly_click").set(d);
       // TODO: provide visual clue that we've selected point(s)
-      // (it's possible this will be handled natively in plotlyjs)
+      // (it's possible this will be handled natively in plotlyjs...)
     });
       
-    grp.var("plotly_click").on('change', function(e) {
+    grp.var('plotly_click').on('change', function(e) {
       console.log(e.value);
       // TODO: if e.value.x !=  e.oldValue.x, then newPlot? Otherwise, restyle?
     });
