b179a87adcd952640adcd26c4b82ed2e67e7e5e7
cpsievert <cpsievert1@gmail.com>
2016-01-28 21:43:36 -0600

    fix plyr/dplyr namespace clash

--- tests/testthat/test-mean-error-bars.R
+++ tests/testthat/test-mean-error-bars.R
@@ -140,7 +140,7 @@ df <- ToothGrowth
 ## conf.interval: the percent range of the confidence interval (default is 95%)
 summarySE <- function(data = NULL, measurevar, groupvars = NULL, na.rm = FALSE,
                       conf.interval = .95, .drop = TRUE) {
-  require(plyr)
+  library(plyr)
   length2 <- function (x, na.rm=FALSE) {
     if (na.rm) sum(!is.na(x))
     else length(x)
@@ -154,7 +154,7 @@ summarySE <- function(data = NULL, measurevar, groupvars = NULL, na.rm = FALSE,
                  },
                  measurevar
   )
-  datac <- rename(datac, c("mean" = measurevar))
+  datac <- plyr::rename(datac, c("mean" = "measurevar"))
   datac$se <- datac$sd / sqrt(datac$N) # Calculate standard error of the mean   
   ciMult <- qt(conf.interval/2 + .5, datac$N-1)
   datac$ci <- datac$se * ciMult
