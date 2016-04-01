e4fc30b8febc31e5c2c57e3ef323737ba54ea0a3
Carson Sievert <cpsievert1@gmail.com>
2016-04-01 15:02:13 +1100

    template requires copying multiple files now

--- tests/testthat.R
+++ tests/testthat.R
@@ -116,9 +116,7 @@ save_outputs <- function(gg, name) {
       dir.create(test_dir, recursive = T)
       # copy over diffing template
       file.copy(
-        file.path(table_dir, "template", "index.html"), 
-        test_dir, 
-        recursive = T
+        dir(file.path(table_dir, "template"), full.names = TRUE), test_dir
       )
       # overwrite the default JSON
       writeLines(
