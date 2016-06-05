d9611aeb90e2d9f5eceafded02d971c1d9e8c6e3
Alexey Stukalov <astukalov@gmail.com>
2016-06-05 21:42:49 +0200

    ggplotly: fix Xaxis anchor if the last row incomplete
    attach X axis to the last non-empty row in a column

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -191,10 +191,12 @@ gg2list <- function(p, width = NULL, height = NULL, tooltip = "all", source = "A
 
   # panel -> plotly.js axis/anchor info
   # (assume a grid layout by default)
-  panel$layout$xaxis <- panel$layout$COL
-  panel$layout$yaxis <- panel$layout$ROW
-  panel$layout$xanchor <- nRows
-  panel$layout$yanchor <- 1
+  panel$layout <- dplyr::mutate( panel$layout,
+    xaxis = COL, yaxis = ROW,
+    yanchor = 1L ) %>%
+    dplyr::group_by( xaxis ) %>%
+    dplyr::mutate( xanchor = max(ROW) ) %>% # anchor X axis to the lowest plot in the column
+    dplyr::ungroup() %>% as.data.frame()
   if (inherits(p$facet, "wrap")) {
     if (p$facet$free$x) {
       panel$layout$xaxis <- panel$layout$PANEL
