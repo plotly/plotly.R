a63ff71a6bb18cf4ad01c5f07b1b046ea2020677
Carson Sievert <cpsievert1@gmail.com>
2016-06-20 14:01:32 -0500

    import/namespace updates

--- R/imports.R
+++ R/imports.R
@@ -3,11 +3,11 @@
 #' @importFrom graphics layout
 #' @importFrom utils getFromNamespace modifyList data packageVersion browseURL
 #' @importFrom stats setNames complete.cases
-#' @importFrom tidyr gather unnest
-#' @importFrom dplyr distinct_ arrange_ group_by_ do
-#' @importFrom viridis viridis
+#' @importFrom tidyr gather gather_ unnest
+#' @importFrom dplyr mutate_ summarise_ select_ arrange_ group_by_ distinct_ slice_ rename_ transmute_ do groups ungroup
+#' @importFrom viridisLite viridis
 #' @importFrom jsonlite toJSON fromJSON
 #' @importFrom httr GET POST PATCH content config add_headers stop_for_status
 #' @importFrom htmlwidgets createWidget sizingPolicy saveWidget
-#' @importFrom lazyeval f_eval f_list dots_capture
+#' @importFrom lazyeval f_eval is_formula all_dots
 NULL
