51b8431f4b477b903be2ea71e56ef47cb5b5547e
Carson Sievert <cpsievert1@gmail.com>
2016-08-05 11:07:18 -0500

    get rid of old shiny code

--- inst/htmlwidgets/plotly.js
+++ inst/htmlwidgets/plotly.js
@@ -44,41 +44,6 @@ HTMLWidgets.widget({
     } else {
       var plot = Plotly.newPlot(graphDiv, x.data, x.layout);
     }
-
-    sendEventData = function(eventType) {
-      return function(eventData) {
-        if (eventData === undefined || !eventData.hasOwnProperty("points")) {
-          return null;
-        }
-        var d = eventData.points.map(function(pt) {
-          var obj = {
-                curveNumber: pt.curveNumber,
-                pointNumber: pt.pointNumber,
-                x: pt.x,
-                y: pt.y
-          };
-          // grab the trace corresponding to this point
-          var tr = x.data[pt.curveNumber];
-          // add on additional trace info, if it exists
-          attachKey = function(keyName) {
-            if (tr.hasOwnProperty(keyName) && tr[keyName] !== null) {
-              if (typeof pt.pointNumber === "number") {
-                obj[keyName] = tr[keyName][pt.pointNumber];
-              } else {
-                obj[keyName] = tr[keyName][pt.pointNumber[0]][pt.pointNumber[1]];
-              }// TODO: can pointNumber be 3D?
-            }
-          };
-          attachKey("z");
-          attachKey("key");
-          return obj;
-        });
-        Shiny.onInputChange(
-          ".clientValue-" + eventType + "-" + x.source,
-          JSON.stringify(d)
-        );
-      };
-    };
     
     // # Begin Crosstalk support
     
@@ -275,33 +240,6 @@ HTMLWidgets.widget({
         graphDiv.removeListener("plotly_deselect", plotly_deselect);
       });
     }
-/*    
-    // TODO: modify event_data() to use crosstalk's R API
-    if (shinyMode) {
-      // https://plot.ly/javascript/zoom-events/
-      graphDiv.on('plotly_relayout', function(d) {
-        Shiny.onInputChange(
-          ".clientValue-" + "plotly_relayout" + "-" + x.source,
-          JSON.stringify(d)
-        );
-      });
-      graphDiv.on('plotly_hover', sendEventData('plotly_hover'));
-      graphDiv.on('plotly_click', sendEventData('plotly_click'));
-      graphDiv.on('plotly_selected', sendEventData('plotly_selected'));
-      graphDiv.on('plotly_unhover', function(eventData) {
-        Shiny.onInputChange(".clientValue-plotly_hover-" + x.source, null);
-      });
-      graphDiv.on('plotly_doubleclick', function(eventData) {
-        Shiny.onInputChange(".clientValue-plotly_click-" + x.source, null);
-      });
-      // 'plotly_deselect' is code for doubleclick when in select mode
-      graphDiv.on('plotly_deselect', function(eventData) {
-        Shiny.onInputChange(".clientValue-plotly_selected-" + x.source, null);
-        Shiny.onInputChange(".clientValue-plotly_click-" + x.source, null);
-      });
-    }
-*/ 
-
   }
 
 });
