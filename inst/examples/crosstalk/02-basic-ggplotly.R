9db0f4d9e13c55fed9cc5e0f28c4e1c00a7353e9
Carson Sievert <cpsievert1@gmail.com>
2016-09-20 18:32:47 -0500

    fix example

--- inst/examples/crosstalk/02-basic-ggplotly.R
+++ inst/examples/crosstalk/02-basic-ggplotly.R
@@ -2,7 +2,6 @@ library(plotly)
 library(crosstalk)
 
 d <- SharedData$new(txhousing, ~city)
-p <- ggplot(d, aes(date, median, group = city)) +
-  geom_line(geom = "line")
+p <- ggplot(d, aes(date, median, group = city)) + geom_line()
 ggplotly(p, tooltip = "city") %>%
   highlight(on = "plotly_hover", color = "red")
