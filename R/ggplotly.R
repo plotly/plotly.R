91b6ffd841b36005c26bf9a70e4c9cac696c8a5f
Carson Sievert <cpsievert1@gmail.com>
2016-10-03 12:40:06 -0500

    fall back on empty string for NULL title. fixes #634.

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -79,7 +79,7 @@ ggplotly.ggmatrix <- function(p = ggplot2::last_plot(), width = NULL,
     subplotList <- c(subplotList, list(s))
   }
   s <- layout(subplot(subplotList, nrows = 1), width = width, height = height)
-  if (nchar(p$title) > 0) {
+  if (nchar(p$title %||% "") > 0) {
     s <- layout(s, title = p$title)
   }
   s
