549fba9e50537333092af56e22ef4b1b07240654
Carson Sievert <cpsievert1@gmail.com>
2016-10-05 11:10:35 -0500

    Add collaborate/cloud args to config for easy adding/removing

--- R/plotly_build.R
+++ R/plotly_build.R
@@ -325,8 +325,7 @@ plotly_build.plotly <- function(p) {
   p <- verify_hovermode(p)
   # try to convert to webgl if toWebGl was used
   p <- verify_webgl(p)
-  # populate R's non-default config
-  p <- config(p)
+  # make sure plots don't get sent out of the network (for enterprise)
   p$x$base_url <- get_domain()
   p
 }
