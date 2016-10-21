15277b7f78eb7778e8a0032829d55cf070d76378
Carson Sievert <cpsievert1@gmail.com>
2016-10-21 00:07:11 -0500

    improve example

--- demo/highlight-binned-target.R
+++ demo/highlight-binned-target.R
@@ -3,12 +3,14 @@ library(crosstalk)
 library(plotly)
 
 d <- SharedData$new(mtcars)
-scatterplot <- plot_ly(d, x = ~mpg, y = ~disp, color = I("black"))
+scatterplot <- plot_ly(d, x = ~mpg, y = ~disp) %>%
+  add_markers(color = I("black")) %>%
+  layout(dragmode = "select")
 
 # add_histogram() does both continuous _and_ discrete binning in the browser,
 # allowing us to perform aggregations on the fly, without 
 p <- subplot(
-  plot_ly(d, x = ~factor(vs)) %>% add_histogram(),
+  plot_ly(d, x = ~factor(cyl)) %>% add_histogram(color = I("black")),
   scatterplot
 ) 
 
@@ -20,5 +22,5 @@ subplot(
   plot_ly(d, y = ~disp, color = I("black")) %>% add_boxplot(name = "overall"),
   scatterplot,
   shareY = TRUE
-)
+) %>% layout(dragmode = "select")
 
