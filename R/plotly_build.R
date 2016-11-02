ba9f840a0317b81540d1bf5a59febebfa0515cba
Carson Sievert <cpsievert1@gmail.com>
2016-11-02 15:42:28 -0500

    don't remove vis mapping attrs

--- R/plotly_build.R
+++ R/plotly_build.R
@@ -294,9 +294,6 @@ plotly_build.plotly <- function(p, registerFrames = TRUE) {
   # (like figures pulled from a plotly server)
   p$x$data <- setNames(c(p$x$data, traces), NULL)
 
-  # get rid of data -> vis mapping stuff
-  p$x[c("visdat", "cur_data", "attrs")] <- NULL
-
   if (has_colorbar(p) && has_legend(p)) {
     if (length(p$x$data) <= 2) {
       p$x$layout$showlegend <- FALSE
