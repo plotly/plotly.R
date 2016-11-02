8deea39c3838edce392d5f7b5dfd5b652dcc58c1
Carson Sievert <cpsievert1@gmail.com>
2016-11-02 15:38:47 -0500

    supply animation options to slider

--- R/animate.R
+++ R/animate.R
@@ -1,5 +1,5 @@
 #' Animation options
-#' 
+#'
 #' @param p a plotly object.
 #' @param frame The amount of time between frames (in milliseconds).
 #' Note that this amount should include the \code{transition}.
@@ -8,7 +8,7 @@
 #' @param easing The type of transition easing. See the list of options here
 #' \url{https://github.com/plotly/plotly.js/blob/master/src/plots/animation_attributes.js}
 #' @param redraw Trigger a redraw of the plot at completion of the transition?
-#' A redraw may significantly impact performance, but may be necessary to 
+#' A redraw may significantly impact performance, but may be necessary to
 #' update plot attributes that can't be transitioned.
 #' @param mode Describes how a new animate call interacts with currently-running
 #' animations. If `immediate`, current animations are interrupted and
@@ -19,41 +19,41 @@
 #' @export
 #' @author Carson Sievert
 #' @seealso \code{\link{animation_slider}()}, \code{\link{animation_button}()}
-#' @examples 
-#' 
-#' 
+#' @examples
+#'
+#'
 #' df <- data.frame(
 #'   x = c(1, 2, 2, 1, 1, 2),
 #'   y = c(1, 2, 2, 1, 1, 2),
 #'   z = c(1, 1, 2, 2, 3, 3)
 #' )
-#' plot_ly(df) %>% 
+#' plot_ly(df) %>%
 #'   add_markers(x = 1.5, y = 1.5) %>%
 #'   add_markers(x = ~x, y = ~y, frame = ~z)
-#' 
-#' 
-#' # it's a good idea to remove smooth transitions when there is 
+#'
+#'
+#' # it's a good idea to remove smooth transitions when there is
 #' # no relationship between objects in each view
 #' plot_ly(mtcars, x = ~wt, y = ~mpg, frame = ~cyl) %>%
 #'   animation_opts(transition = 0)
-#'  
+#'
 #' # works the same way with ggplotly
-#' p <- ggplot(txhousing, aes(month, median)) + 
+#' p <- ggplot(txhousing, aes(month, median)) +
 #'   geom_smooth() +
-#'   geom_line(aes(frame = year, ids = month)) + 
+#'   geom_line(aes(frame = year, ids = month)) +
 #'   facet_wrap(~ city)
-#'  
-#' ggplotly(p, width = 1000, height = 500) %>% 
+#'
+#' ggplotly(p, width = 1000, height = 500) %>%
 #'   animation_slider(hide = TRUE)
-#' 
+#'
 #' # use the ids attribute to ensure object constancy
 #' if (require("gapminder")) {
-#'   p <- ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, 
+#'   p <- ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop,
 #'     color = continent, frame = year, ids = country)) +
 #'     geom_point() +
 #'     scale_x_log10()
 #'   ggplotly(p)
-#'   
+#'
 #'   p2 <- ggplot(gapminder, aes(gdpPercap, lifeExp)) +
 #'     geom_point(aes(size = pop), alpha = 0.5) +
 #'     # animations can be specified on the layer level
@@ -61,7 +61,7 @@
 #'     scale_x_log10()
 #'   ggplotly(p2)
 #' }
-#' 
+#'
 animation_opts <- function(p, frame = 500, transition = frame, easing = "linear",
                            redraw = FALSE, mode = "immediate") {
   if (frame < 0) {
@@ -73,7 +73,7 @@ animation_opts <- function(p, frame = 500, transition = frame, easing = "linear"
   if (frame < transition) {
     stop("frame must be larger than transition", call. = FALSE)
   }
-  
+
   opts <- list(
     transition = list(
       duration = transition,
@@ -85,17 +85,17 @@ animation_opts <- function(p, frame = 500, transition = frame, easing = "linear"
     ),
     mode = match.arg(mode, c('immediate', 'next', 'afterall'))
   )
-  
+
   # build step will ensure we can access the animation frames
   # (required to fill the steps in correctly)
   p <- plotly_build(p)
-  
+
   # overwrite the animation options in the slider/button spec
-  supply_ani_slider(supply_ani_button(p, opts))
+  supply_ani_slider(supply_ani_button(p, opts = opts), opts = opts)
 }
 
 #' Hide or customize the animation button
-#' 
+#'
 #' @param p a plotly object
 #' @param hide remove the animation slider?
 #' @param ... attributes passed to the sliders object which controls the animation
@@ -103,10 +103,10 @@ animation_opts <- function(p, frame = 500, transition = frame, easing = "linear"
 #' @export
 #' @author Carson Sievert
 #' @seealso \code{\link{animation_opts}()}, \code{\link{animation_button}()}
-#' 
+#'
 
 animation_slider <- function(p, hide = FALSE, ...) {
-  
+
   p <- plotly_build(p)
   isAniSlider <- vapply(p$x$layout$sliders, is_ani_slider, logical(1))
   if (hide) {
@@ -117,21 +117,21 @@ animation_slider <- function(p, hide = FALSE, ...) {
     p$x$layout$sliders[[which(isAniSlider)]], list(...)
   )
   p
-  
+
 }
 
 #' Hide or customize the animation button
-#' 
+#'
 #' @param p a plotly object
 #' @param ... arguments passed to the updatemenus which controls the play/pause
 #' button \url{https://github.com/plotly/plotly.js/blob/master/src/components/updatemenus/attributes.js}
 #' @export
 #' @author Carson Sievert
 #' @seealso \code{\link{animation_opts}()}, \code{\link{animation_button}()}
-#' 
+#'
 
 animation_button <- function(p, ...) {
-  
+
   p <- plotly_build(p)
   isAniButton <- vapply(p$x$layout$updatemenus, is_ani_button, logical(1))
   p$x$layout$updatemenus[[which(isAniButton)]] <- modify_list(
@@ -141,7 +141,7 @@ animation_button <- function(p, ...) {
 }
 
 
-# supply an animation button if it doesn't exist, 
+# supply an animation button if it doesn't exist,
 # and _replace_ an existing animation button
 supply_ani_button <- function(p, opts = NULL) {
   nmenus <- length(p$x$layout$updatemenus)
@@ -177,7 +177,7 @@ is_ani_button <- function(obj) {
   class(obj) %in% "aniButton"
 }
 
-# supply an animation slider if it doesn't exist, 
+# supply an animation slider if it doesn't exist,
 # and _replace_ an existing animation slider
 supply_ani_slider <- function(p, opts = NULL, ...) {
   nsliders <- length(p$x$layout$sliders)
@@ -197,13 +197,16 @@ create_ani_slider <- function(p, opts = NULL, ...) {
     args[[2]] <- opts
     list(method = "animate", args = args, label = nm, value = nm)
   })
-  
+
   # inherit defaults from any existing slider
   slider <- modify_list(
     p$x$layout$sliders[[vapply(p$x$layout$sliders, is_ani_slider, logical(1))]], list(...)
   )
-  slider$visible <- TRUE
-  slider$steps <- slider[["steps"]] %||% steps
+  # don't let the user override steps
+  slider$steps <- steps
+
+  # set some opinionated defaults
+  slider$visible <- slider$visible %||% TRUE
   slider$pad$t <- slider$pad[["t"]] %||% 40
   structure(slider, class = "aniSlider")
 }
@@ -214,11 +217,11 @@ is_ani_slider <- function(obj) {
 
 
 easingOpts <- function() {
-  c('linear', 'quad', 'cubic', 'sin', 'exp', 'circle', 'elastic', 'back', 
-    'bounce', 'linear-in', 'quad-in', 'cubic-in', 'sin-in', 'exp-in', 
-    'circle-in', 'elastic-in', 'back-in', 'bounce-in', 'linear-out', 
-    'quad-out', 'cubic-out', 'sin-out', 'exp-out', 'circle-out', 'elastic-out', 
-    'back-out', 'bounce-out', 'linear-in-out', 'quad-in-out', 'cubic-in-out', 
-    'sin-in-out', 'exp-in-out', 'circle-in-out', 'elastic-in-out', 
-    'back-in-out', 'bounce-in-out') 
+  c('linear', 'quad', 'cubic', 'sin', 'exp', 'circle', 'elastic', 'back',
+    'bounce', 'linear-in', 'quad-in', 'cubic-in', 'sin-in', 'exp-in',
+    'circle-in', 'elastic-in', 'back-in', 'bounce-in', 'linear-out',
+    'quad-out', 'cubic-out', 'sin-out', 'exp-out', 'circle-out', 'elastic-out',
+    'back-out', 'bounce-out', 'linear-in-out', 'quad-in-out', 'cubic-in-out',
+    'sin-in-out', 'exp-in-out', 'circle-in-out', 'elastic-in-out',
+    'back-in-out', 'bounce-in-out')
 }
