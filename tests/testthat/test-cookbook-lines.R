7d96241e923bdd7bbf48ffd35a87647d2905a1a9
Carson Sievert <cpsievert1@gmail.com>
2017-01-23 15:17:56 -0600

    test fixes

--- tests/testthat/test-cookbook-lines.R
+++ tests/testthat/test-cookbook-lines.R
@@ -185,7 +185,7 @@ test_that("geom_hline -> 2 more traces", {
   info <- expect_traces(temp, 4, "scatter-facet-hline")
   
   expect_true(info$layout$showlegend)
-  has.name <- sapply(info$data, function(tr) nchar(tr$name) > 0)
+  has.name <- sapply(info$data, function(tr) isTRUE(nchar(tr$name) > 0))
   expect_equal(sum(has.name), 2)
 })
 
