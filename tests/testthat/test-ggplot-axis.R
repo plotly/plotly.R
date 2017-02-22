92ba0eb0a83b4cbf1e99168da38496232098c355
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2017-02-22 19:18:25 +0000

    add tests

--- tests/testthat/test-ggplot-axis.R
+++ tests/testthat/test-ggplot-axis.R
@@ -1,4 +1,4 @@
-context("axes")
+context("Axis moving")
 
 expect_traces <- function(gg, n.traces, name){
   stopifnot(is.numeric(n.traces))
@@ -13,27 +13,69 @@ expect_traces <- function(gg, n.traces, name){
 }
 
 p <- ggplot(mtcars, aes(x=mpg, y=wt)) + 
-	geom_point() + 
-	scale_x_continuous(position="top") + 
-	scale_y_continuous(position="right")
+	geom_point()
 
+# p3 <- p + facet_wrap(~carb)
 
-test_that("Axis positions move to top and right", {
-  info <- save_outputs(p, "axis_move")
+test_that("Axis position moves to top", {
+	p <- p + scale_x_continuous(position="top"))
+  
+  info <- save_outputs(p, "axis_move_top")
   expect_equal(length(info$data), 1)
   expect_identical(info$layout$xaxis$side, "top")
-  expect_identical(info$layout$yaxis$side, "right")
-  expect_traces(p, 1, "move")
 })
 
+test_that("Axis position moves to right", {
+  p <- p + scale_y_continuous(position="right")
+  
+  info <- save_outputs(p, "axis_move_right")
+  expect_equal(length(info$data), 1)
+  expect_identical(info$layout$xaxis$side, "top")
+})
 
-p <- ggplot(mtcars, aes(x=mpg, y=wt)) + 
-	geom_point()
+test_that("Axis position moves to top (facets)", {
+  p <- p + scale_x_continuous(position="top")) + facet_wrap(~carb)
+
+  info <- save_outputs(p, "axis_move_top_facet")
+  expect_equal(length(info$data), 1)
+  expect_equal(info$data$xaxis$anchor, "y1")
+  expect_identical(info$layout$xaxis$side, "top")
+})
+
+test_that("Axis position moves to top (facets)", {
+  p <- p + scale_y_continuous(position="right")) + facet_wrap(~carb)
+
+  info <- save_outputs(p, "axis_move_right_facet")
+  
+  expect_equal(length(info$data), 1)
+
+  expect_equal(info$data$yaxis$anchor, "x3")
+  expect_identical(info$layout$yaxis$side, "right")
+})
 
 test_that("Axis positions stay at bottom and left", {
   info <- save_outputs(p, "axis_stay")
+  
   expect_equal(length(info$data), 1)
+  
   expect_identical(info$layout$xaxis$side, "bottom")
   expect_identical(info$layout$yaxis$side, "left")
-  expect_traces(p, 1, "stay")
+
+  expect_equal(info$layout$xaxis$anchor, "y")
+  expect_equal(info$layout$yaxis$anchor, "x")
 })
+
+
+test_that("Axis positions stay at bottom and left (facet)", {
+  p <- p + facet_wrap(~carb)
+  info <- save_outputs(p, "axis_stay")
+  
+  expect_equal(length(info$data), 1)
+  
+  expect_identical(info$layout$xaxis$side, "bottom")
+  expect_identical(info$layout$yaxis$side, "left")
+
+  expect_equal(info$layout$xaxis$anchor, "y")
+  expect_equal(info$layout$yaxis$anchor, "x")
+})
+
