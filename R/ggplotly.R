03bc6b130239a3c549f94904b21ee14e362411ae
Carson Sievert <cpsievert1@gmail.com>
2017-04-24 17:23:30 -0500

    fix for inverse transformation of dates

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -648,15 +648,24 @@ gg2list <- function(p, width = NULL, height = NULL,
       )
       
       # inverse transform date data based on tickvals/ticktext
+      invert_date <- function(x, scale) {
+        if (inherits(scale, "ScaleContinuousDatetime")) {
+          as.POSIXct(x, origin = "1970-01-01", tz = scale$timezone)
+        } else {
+          as.Date(x, origin = "1970-01-01", tz = scale$timezone)
+        }
+      }
+      
+      
+      
       if (isDateType) {
-        as_date <- function(x) as.POSIXct(x, origin="1970-01-01", tz=sc$timezone)
-        axisObj$range <- as_date(axisObj$range)
+        axisObj$range <- invert_date(axisObj$range, sc)
         traces <- lapply(traces, function(tr) {
-          tr[[xy]] <- as_date(tr[[xy]])
+          tr[[xy]] <- invert_date(tr[[xy]], sc)
           # TODO: are there other similar cases we need to handle?
           if (identical("bar", tr$type)) {
-            tr[["width"]] <- as_date(tr[["width"]])
-            tr[["base"]] <- as_date(tr[["base"]])
+            tr[["width"]] <- invert_date(tr[["width"]], sc)
+            tr[["base"]] <- invert_date(tr[["base"]], sc)
           }
           tr
         })
