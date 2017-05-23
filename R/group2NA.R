86dfc9aa5293ceea33f49aafad8d4da0d8282491
Carson Sievert <cpsievert1@gmail.com>
2017-05-23 10:37:31 -0500

    export group2NA() and provide better documentation

--- R/group2NA.R
+++ R/group2NA.R
@@ -1,26 +1,44 @@
-#' Insert missing values to create trace groupings
+#' Separate groups with missing values
 #' 
-#' If a group of traces share the same non-positional characteristics (i.e.,
-#' color, fill, etc), it is more efficient to draw them as a single trace 
-#' with missing values that separate the groups (instead of multiple traces).
-#' This is a helper function for inserting missing values into a data set
+#' This function is used internally by plotly, but may also be useful to some 
+#' power users. The details section explains when and why this function is useful.
+#' 
+#' @details If a group of scatter traces share the same non-positional characteristics 
+#' (i.e., color, fill, etc), it is more efficient to draw them as a single trace 
+#' with missing values that separate the groups (instead of multiple traces), 
+#' In this case, one should also take care to make sure 
+#' \href{https://plot.ly/r/reference/#scatter-connectgaps}{connectgaps} 
+#' is set to \code{FALSE}.
 #' 
 #' @param data a data frame.
-#' @param groupNames name(s) of the grouping variable(s) as a character vector
+#' @param groupNames character vector of grouping variable(s)
 #' @param nested other variables that group should be nested 
 #' (i.e., ordered) within.
 #' @param ordered a variable to arrange by (within nested & groupNames). This
 #' is useful primarily for ordering by x
 #' @param retrace.first should the first row of each group be appended to the 
 #' last row? This is useful for enclosing polygons with lines.
+#' @export
+#' @return a data.frame with rows ordered by: \code{nested}, 
+#' then \code{groupNames}, then \code{ordered}. As long as \code{groupNames} 
+#' contains valid variable names, new rows will also be inserted to separate 
+#' the groups.
 #' @examples 
 #' 
+#' # note the insertion of new rows with missing values 
 #' group2NA(mtcars, "vs", "cyl")
 #' 
-#' elong <- tidyr::gather(economics, variable, value, -date)
-#' plot_ly(group2NA(elong, "variable"), x = ~date, y = ~value)
+#' # need to group lines by city somehow!
+#' plot_ly(txhousing, x = ~date, y = ~median) %>% add_lines()
 #' 
-#'
+#' # instead of using group_by(), you could use group2NA()
+#' tx <- group2NA(txhousing, "city")
+#' plot_ly(tx, x = ~date, y = ~median) %>% add_lines()
+#' 
+#' # add_lines() will ensure paths are sorted by x, but this is equivalent
+#' tx <- group2NA(txhousing, "city", ordered = "date")
+#' plot_ly(tx, x = ~date, y = ~median) %>% add_paths()
+#' 
 
 group2NA <- function(data, groupNames = "group", nested = NULL, ordered = NULL,
                      retrace.first = inherits(data, "GeomPolygon")) {
