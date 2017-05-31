20b4f4b1a0ce5cda157ad9cf86ed17cf8baa9544
Carson Sievert <cpsievert1@gmail.com>
2017-05-31 16:40:46 -0500

    Introducing plotlyProxy and plotlyProxyInvoke; fixes #580

---
+++ R/proxy.R
@@ -0,0 +1,101 @@
+#' Modify a plotly object inside a shiny app
+#'
+#' @param outputId single-element character vector indicating the output ID
+#'   map to modify (if invoked from a Shiny module, the namespace will be added
+#'   automatically)
+#' @param session the Shiny session object to which the map belongs; usually the
+#'   default value will suffice.
+#' @param deferUntilFlush indicates whether actions performed against this
+#'   instance should be carried out right away, or whether they should be held
+#'   until after the next time all of the outputs are updated.
+#'
+#' @rdname plotlyProxy
+#' @export 
+#' @examples
+#' 
+#' demo("proxy-mapbox", package = "plotly")
+#' demo("proxy-relayout", package = "plotly")
+#' 
+
+plotlyProxy <- function(outputId, session = shiny::getDefaultReactiveDomain(), 
+                        deferUntilFlush = TRUE) {
+  
+  # implementation very similar to leaflet::leafletProxy & DT:dataTableProxy
+  if (is.null(session)) {
+    stop("plotlyProxy must be called from the server function of a Shiny app")
+  }
+  if (!is.null(session$ns) && nzchar(session$ns(NULL)) && 
+      !startsWith(outputId, session$ns(""))) {
+    outputId <- session$ns(outputId)
+  }
+  structure(
+    list(
+      session = session, 
+      id = outputId,
+      deferUntilFlush = deferUntilFlush
+      # TODO: is there actually a use-case for this?
+      #x = structure(list(), leafletData = data), 
+      #dependencies = NULL
+    ), 
+    class = "plotly_proxy"
+  )
+}
+
+
+#' @param p a plotly proxy object (created with \code{plotlyProxy})
+#' @param method a plotlyjs method to invoke. For a list of options,
+#' visit the \href{https://plot.ly/javascript/plotlyjs-function-reference}
+#' {plotlyjs function reference}
+#' @param ... unnamed arguments passed onto 
+#' @rdname plotlyProxy
+#' @export
+plotlyProxyInvoke <- function(p, method, ...) {
+  
+  if (!is.proxy(p))
+    stop("p must be a proxy object. See `help(plotlyProxy)`", call. = FALSE)
+  
+  if (missing(method))
+    stop(
+      "Must provide a plotly.js method (as a character string of length 1).\n", 
+      sprintf("Valid options include: '%s'", 
+              paste(plotlyjs_methods(), collapse = "', '")),
+      call. = FALSE
+    )
+  
+  method <- match.arg(method, plotlyjs_methods())
+  
+  msg <- list(
+    id = p$id,
+    method = method,
+    # TODO: can we leverage the plotly_build() infrastructure in a smart way?
+    #  args = evalFormula(list(...), data)
+    args = list(...)
+  )
+  
+  if (isTRUE(p$deferUntilFlushed)) {
+    
+    p$session$onFlushed(function() {
+      p$session$sendCustomMessage("plotly-calls", msg)
+    }, once = TRUE)
+    
+  } else {
+    
+    p$session$sendCustomMessage("plotly-calls", msg)
+    
+  }
+  
+  p
+}
+
+
+plotlyjs_methods <- function() {
+  c(
+    "restyle", "relayout", "update", "addTraces", "deleteTraces", "moveTraces",
+    "extendTraces", "prependTraces", "purge", "toImage", "downloadImage"
+  )
+}
+
+
+is.proxy <- function(x) {
+  inherits(x, "plotly_proxy")
+}
