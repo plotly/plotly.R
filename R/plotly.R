a55da98c48a31dff89628b1971c0d5783f58ccd3
Carson Sievert <cpsievert1@gmail.com>
2017-10-16 12:49:02 -0500

    upgrade to plotly.js v1.31.1
    also leverage newly distributed plot schema to avoid terrible manual update process

--- R/plotly.R
+++ R/plotly.R
@@ -373,7 +373,7 @@ typedArrayPolyfill <- function() {
 # and bundle size at print time.
 plotlyMainBundle <- function() {
   htmltools::htmlDependency(
-    "plotlyjs", "1.31.0",
+    "plotlyjs", "1.31.1",
     src = depPath("plotlyjs"),
     script = "plotly-latest.min.js",
     stylesheet = "plotly-htmlwidgets.css"
