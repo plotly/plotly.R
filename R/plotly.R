5fd65c46ae274871a6527e83beb4724b8581e780
Carson Sievert <cpsievert1@gmail.com>
2018-03-07 16:51:55 -0600

    upgrade to plotlyjs v1.35.0

--- R/plotly.R
+++ R/plotly.R
@@ -373,7 +373,7 @@ typedArrayPolyfill <- function() {
 # and bundle size at print time.
 plotlyMainBundle <- function() {
   htmltools::htmlDependency(
-    "plotlyjs", "1.34.0",
+    "plotlyjs", "1.35.0",
     src = depPath("plotlyjs"),
     script = "plotly-latest.min.js",
     stylesheet = "plotly-htmlwidgets.css"
