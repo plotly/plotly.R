17fd25b50513021e3026ca12c9dd745cd92e6d82
Carson Sievert <cpsievert1@gmail.com>
2018-03-29 16:20:00 -0500

    add pie test of stroke/span/color/size

--- tests/testthat/test-plotly-pie.R
+++ tests/testthat/test-plotly-pie.R
@@ -23,3 +23,17 @@ test_that("No cartesian axes are supplied to a pie chart", {
 })
 
 
+
+test_that("stroke/span controls slice outline; color/size controls text", {
+  p <- plot_ly(ds, labels = ~labels, values = ~values) %>%
+    add_pie(color = I("white"), size = I(20), stroke = I("black"), span = I(5)) 
+  
+  l <- plotly_build(p)$x
+  
+  expect_true(all(l$data[[1]]$labels == ds$labels))
+  expect_true(all(l$data[[1]]$values == ds$values))
+  expect_true(l$data[[1]]$textfont$color == toRGB("white"))
+  expect_true(l$data[[1]]$textfont$size == 20)
+  expect_true(l$data[[1]]$marker$line$color == toRGB("black"))
+  expect_true(l$data[[1]]$marker$line$width == 5)
+})
