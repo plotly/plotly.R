a1a79f553e970b21566f4c7c79882285933e95e1
Carson Sievert <cpsievert1@gmail.com>
2018-09-20 15:44:37 -0500

    add support for partial updates in style(), fixes #1342

--- R/style.R
+++ R/style.R
@@ -17,6 +17,12 @@
 #' # keep the hover info for points, but remove it for the line/ribbon
 #' style(p, hoverinfo = "none", traces = c(2, 3))
 #' 
+#' 
+#' # to turn the marker's red, without destroying the marker's other properties
+#' # https://github.com/plotly/plotly.js/issues/1866#issuecomment-314115744
+#' style(p, marker.color = "red", traces = 1) 
+#' style(p, marker.line.color = "red", traces = 1) 
+#' 
 style <- function(p, ..., traces = NULL) {
   p <- plotly_build(p)
   nTraces <- length(p$x$data)
@@ -25,10 +31,30 @@ style <- function(p, ..., traces = NULL) {
   traces <- traces[!idx]
   if (any(idx)) warning("You've referenced non-existent traces", call. = FALSE)
   argz <- list(...)
+  
+  # argument names that contain a '.' signify a "partial update"
+  isPartialUpdate <- grepl("\\.", names(argz))
+  # expand these special arguments to a suitable list object,
+  # `list(marker.color = "red")`, to `list(marker = list(color = "red"))`
+  nms <- strsplit(names(argz), "\\.")
+  for (i in seq_along(nms)) {
+    nm <- nms[[i]]
+    if (length(nm) == 1) next
+    val <- argz[[i]]
+    for (j in seq(length(nm), 2)) {
+      val <- setNames(list(val), nm[j])
+    }
+    argz[[i]] <- val
+  }
+  argz <- setNames(argz, unlist(lapply(nms, `[[`, 1)))
+  
+  # perform the replacement
   for (i in traces) {
-    for (j in names(argz)) {
-      p$x$data[[i]][[j]] <- argz[[j]]
+    for (j in seq_along(argz)) {
+      attr <- names(argz)[j]
+      p$x$data[[i]][[attr]] <- if (isPartialUpdate[j]) modify_list(p$x$data[[i]][[attr]], argz[[attr]]) else argz[[attr]]
     }
   }
+  
   p
 }
---
+++ tests/testthat/test-style.R
@@ -0,0 +1,25 @@
+context("style/restyle functionality")
+
+p1 <- plot_ly(x = 1:10, y = 1:10, symbol = I(15))
+marker1 <- plotly_build(p)$x$data[[1]]$marker
+
+test_that("Whole update works as expected", {
+  p2 <- style(p1, marker = list(color = "red"))
+  marker2 <- plotly_build(p2)$x$data[[1]]$marker
+  expect_equal(marker2, list(color = "red"))
+  
+  p3 <- style(p1, marker = list(line = list(color = "red", width = 10)))
+  marker3 <- plotly_build(p3)$x$data[[1]]$marker
+  expect_equal(marker3, list(line = list(color = "red", width = 10)))
+})
+
+
+test_that("Partial update works as expected", {
+  p4 <- style(p, marker.color = "red")
+  marker4 <- plotly_build(p4)$x$data[[1]]$marker
+  expect_equal(marker4, modifyList(marker4, list(color = "red")))
+  
+  p5 <- style(p, marker.line.color = "red")
+  marker5 <- plotly_build(p5)$x$data[[1]]$marker
+  expect_equal(marker5, modifyList(marker5, list(line = list(color = "red"))))
+})
