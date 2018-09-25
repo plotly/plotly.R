a852a222c8e8f6b088c8506b50c540e57d4ebbb1
Carson Sievert <cpsievert1@gmail.com>
2018-09-25 16:34:52 -0500

    adopt ggplot2's strategy for determining when vdiffr should run

--- tests/testthat/helper-vdiffr.R
+++ tests/testthat/helper-vdiffr.R
@@ -1,6 +1,26 @@
+# default is equal to whether NOT_CRAN is true or not
+enable_vdiffr <- identical(Sys.getenv("NOT_CRAN"), "true")
 
-save_outputs <- function(gg, name) {
-  p <- plotly_build(gg)$x[c("data", "layout")]
-  vdiffr::expect_doppelganger(name, p)
-  invisible(p)
+# disable or enable vdiffr based on the state of USE_VDIFFR, if set
+if (identical(Sys.getenv("USE_VDIFFR"), "true")) {
+  enable_vdiffr <- TRUE
+} else if (identical(Sys.getenv("USE_VDIFFR"), "false")) {
+  enable_vdiffr <- FALSE
+}
+
+# disable vdiffr if version is old
+if (!requireNamespace("vdiffr", quietly = TRUE) ||
+    utils::packageVersion("vdiffr") < "0.2.3.9001") {
+  enable_vdiffr <- FALSE
+}
+
+
+expect_doppelganger <- function(p, name, ...) {
+  
+  if (enable_vdiffr) {
+    ignore <- vdiffr::expect_doppelganger(name, p, ...)
+  }
+  # return 'built' data/layout
+  
+  plotly_build(p)$x[c("data", "layout")]
 }
