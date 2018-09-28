3f51097c86506d0c9e4260df3908d8f4849e8852
Carson Sievert <cpsievert1@gmail.com>
2018-09-28 16:59:21 -0500

    skip api tests if there is no plotly username/account

--- tests/testthat/helper-skip.R
+++ tests/testthat/helper-skip.R
@@ -3,6 +3,9 @@
 # skipped on pull requests (the .travis.yml file uses encrypted credentials
 # & encrypted environment vars cannot be accessed on pull request builds)
 skip_if_not_master <- function() {
+  if (is.na(Sys.getenv("plotly_username", NA))) {
+    return(skip("Testing plot.ly API calls requires a plotly account"))
+  }
   is_pr <- grepl("^[0-9]+$", Sys.getenv("TRAVIS_PULL_REQUEST"))
   is_r_release <- Sys.getenv("TRAVIS_R_VERSION_STRING", "release") == "release"
   if (!is_pr && is_r_release) return(invisible(TRUE))
