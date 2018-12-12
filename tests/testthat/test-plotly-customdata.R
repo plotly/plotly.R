cdd48e242e9591a845b3699e1d7d7b182d05d986
Carson Sievert <cpsievert1@gmail.com>
2018-12-12 14:45:07 -0600

    better support for customdata list-columns

--- tests/testthat/test-plotly-customdata.R
+++ tests/testthat/test-plotly-customdata.R
@@ -8,3 +8,25 @@ test_that("ggplotly relays customdata", {
   trace <- l$x$data[[1]]
   expect_equivalent(trace$customdata, nms)
 })
+
+
+test_that("Can provide list-columns to customdata", {
+  l <- txhousing %>%
+    group_by(city) %>%
+    highlight_key(~city) %>%
+    plot_ly(x = ~date, y = ~median, hoverinfo = "name") %>%
+    add_lines(customdata = ~map2(date, median, ~list(.x, .y))) %>%
+    plotly_build()
+  
+  trace <- l$x$data[[1]]
+  expect_true(length(trace$customdata) == length(trace$x))
+  
+  # make sure customdata have been arranged properly
+  customx <- unlist(lapply(trace$customdata, function(x) x[1] %||% NA))
+  expect_equivalent(customx, trace$x)
+  
+  # check there is no customdata where x values are null
+  nullcd <- trace$customdata[which(is.na(trace$x))]
+  expect_true(unique(lengths(nullcd)) == 0)
+})
+
