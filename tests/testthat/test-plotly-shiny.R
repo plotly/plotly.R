89296a5308a0666f3119d3b0c1a475cf62b493ef
Carson Sievert <cpsievert1@gmail.com>
2019-01-25 15:19:58 -0600

    fix test expectations

--- tests/testthat/test-plotly-shiny.R
+++ tests/testthat/test-plotly-shiny.R
@@ -4,20 +4,20 @@ context("shiny")
 test_that("event_register() registers an event", {
   p <- plotly_build(plot_ly(x = 1))
   
-  expect_false("plotly_selecting" %in% p$x$shinyInputs)
+  expect_false("plotly_selecting" %in% p$x$shinyEvents)
   
   p <- event_register(p, "plotly_selecting")
   
-  expect_true("plotly_selecting" %in% p$x$shinyInputs)
+  expect_true("plotly_selecting" %in% p$x$shinyEvents)
 })
 
 
 test_that("event_unregister() de-registers an event", {
   p <- plotly_build(plot_ly(x = 1))
   
-  expect_true("plotly_selected" %in% p$x$shinyInputs)
+  expect_true("plotly_selected" %in% p$x$shinyEvents)
   
   p <- event_unregister(p, "plotly_selected")
   
-  expect_false("plotly_selected" %in% p$x$shinyInputs)
+  expect_false("plotly_selected" %in% p$x$shinyEvents)
 })
