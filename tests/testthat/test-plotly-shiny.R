fe2684a75cba8896d67a2cc6f941da8d058edc4d
Carson Sievert <cpsievert1@gmail.com>
2019-01-29 11:50:07 -0600

    actually, no really, disable shinytest in vdiffr run

--- tests/testthat/test-plotly-shiny.R
+++ tests/testthat/test-plotly-shiny.R
@@ -26,7 +26,7 @@ test_that("event_unregister() de-registers an event", {
 test_that("event_data shiny app works", {
   skip_on_cran()
   skip_if_not_installed("shinytest")
-  if (!enable_vdiffr) skip("shiny testing not performed during visual testing")
+  if (enable_vdiffr) skip("shiny testing not performed during visual testing")
   
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
