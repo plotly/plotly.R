ac57d3691e5c3b98afbc04be6aeeb234c2ada0ef
Carson Sievert <cpsievert1@gmail.com>
2019-04-02 17:44:07 -0500

    re-surface an old test

--- tests/testthat/test-ggplot-labels.R
+++ tests/testthat/test-ggplot-labels.R
@@ -17,15 +17,14 @@ test_that("ylab is translated correctly", {
   expect_identical(labs, c("Petal.Width", "sepal width"))
 })
 
-# TODO: why is this failing on R-devel???
-#test_that("scale_x_continuous(name) is translated correctly", {
-#  ggiris <- ggplot(iris) +
-#    geom_point(aes(Petal.Width, Sepal.Width)) +
-#    scale_x_continuous("petal width")
-#  info <- expect_doppelganger_built(ggiris, "labels-scale_x_continuous_name")
-#  labs <- unlist(lapply(info$layout$annotations, "[[", "text"))
-#  expect_identical(sort(labs), c("petal width", "Sepal.Width"))
-#})
+test_that("scale_x_continuous(name) is translated correctly", {
+  ggiris <- ggplot(iris) +
+    geom_point(aes(Petal.Width, Sepal.Width)) +
+    scale_x_continuous("petal width")
+  info <- expect_doppelganger_built(ggiris, "labels-scale_x_continuous_name")
+  labs <- c(info$layout$xaxis$title$text, info$layout$yaxis$title$text)
+  expect_identical(labs, c("petal width", "Sepal.Width"))
+})
 
 test_that("angled ticks are translated correctly", {
   ggiris <- ggplot(iris) +
