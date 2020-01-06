e85ee24394893792b1b39fe3881d73cdc107924f
Carson <cpsievert1@gmail.com>
2020-01-06 17:50:16 -0600

    link to comment

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -686,6 +686,7 @@ gg2list <- function(p, width = NULL, height = NULL,
       ticktext <- rng[[xy]]$get_labels %()% rng[[paste0(xy, ".labels")]]
       tickvals <- rng[[xy]]$break_positions %()% rng[[paste0(xy, ".major")]]
       
+      # https://github.com/tidyverse/ggplot2/pull/3566#issuecomment-565085809
       ticktext <- ticktext[!is.na(ticktext)]
       tickvals <- tickvals[!is.na(tickvals)]
       
