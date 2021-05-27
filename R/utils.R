8eae641881df6e9657294a229ab3acc18455a9fe
chschan <chschan@users.noreply.github.com>
2021-05-27 14:32:10 +1000

    Add back missing layout attribute

--- R/utils.R
+++ R/utils.R
@@ -449,7 +449,7 @@ verify_attr_names <- function(p) {
   # some layout attributes (e.g., [x-y]axis can have trailing numbers)
   attrs_name_check(
     sub("[0-9]+$", "", names(p$x$layout)),
-    c(names(Schema$layout$layoutAttributes), c("boxmode", "barmode", "bargap", "mapType")),
+    c(names(Schema$layout$layoutAttributes), c("boxmode", "barmode", "bargap", "bargroupgap", "mapType")),
     "layout"
   )
   attrs_name_check(
