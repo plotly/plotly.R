416c9d17a5b5c1abf2aefe1229ea2cb45a0414f9
Carson <cpsievert1@gmail.com>
2021-06-08 17:06:54 -0500

    bump mathjax version

--- inst/htmlwidgets/lib/mathjax/cdn.js
+++ inst/htmlwidgets/lib/mathjax/cdn.js
@@ -1,4 +1,4 @@
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-AMS-MML_SVG";
+script.src  = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_SVG";
 document.getElementsByTagName("head")[0].appendChild(script);
