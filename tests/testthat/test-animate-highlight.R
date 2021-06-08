8787e85a4c87c475de07341b0ea0de990948161c
Carson <cpsievert1@gmail.com>
2021-06-08 10:56:12 -0500

    Start v4.9.4 release candidate

--- tests/testthat/test-animate-highlight.R
+++ tests/testthat/test-animate-highlight.R
@@ -391,6 +391,7 @@ test_that("animation button can be customized", {
 
 test_that("sf works with crosstalk", {
   skip_if_not_installed("sf")
+  skip_if_not_installed("s2")
   
   nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE)
   # shared data will make the polygons "query-able"
