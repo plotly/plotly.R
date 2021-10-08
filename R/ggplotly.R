86b244bd861b5f06ef1ad4d2ff6048485fa19513
Abdessabour Moutik <abds.papa@gmail.com>
2021-10-08 09:03:38 -0700

    Allowed for user defined axis positions. Closes #2029

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -753,6 +753,7 @@ gg2list <- function(p, width = NULL, height = NULL,
         gridwidth = unitConvert(panelGrid, "pixels", type),
         zeroline = FALSE,
         anchor = anchor,
+        side = scales$get_scales(xy)$position,
         # layout.axisid.title don't yet support alignment :(
         title = list(
           text = faced(axisTitleText, axisTitle$face),
