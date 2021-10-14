6d29badaa0ebe52fd53af6c2de9fc33af5dfac83
Abdessabour Moutik <abds.papa@gmail.com>
2021-10-14 14:10:42 -0700

    Removing `TeX` class easily
    Co-authored-by: Carson Sievert <cpsievert1@gmail.com>

--- R/ggplotly.R
+++ R/ggplotly.R
@@ -1302,7 +1302,7 @@ italic <- function(x) paste("<i>", x, "</i>")
 
 # if a vector that has one unique value (ignoring missings), return that value
 uniq <- function(x) {
-  if("TeX" %in% class(x)) x <- as.character(x)
+  x <- remove_class(x, "TeX")
   u <- unique(x)
   if (identical(u, NA) || length(u) == 0) return(u)
   u <- u[!is.na(u)]
