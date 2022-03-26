0661fb263bd9da24b479c0f43c33609e73303cbd
Carson <cpsievert1@gmail.com>
2022-03-26 13:15:48 -0500

    Use rlang::local_options() in test

--- tests/testthat/test-ggplot-warnings.R
+++ tests/testthat/test-ggplot-warnings.R
@@ -2,6 +2,6 @@
 
 test_that("ggplotly does not issue partial-argument-match warning", {
   p <- ggplot(data.frame())
-  rlang::scoped_options(warnPartialMatchArgs = TRUE)
+  rlang::local_options(warnPartialMatchArgs = TRUE)
   expect_warning(ggplotly(p), regexp = NA)
 })
