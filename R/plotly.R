3c3d749e41f40064454ebd4db506bb84a95a1ccf
Carson Sievert <cpsievert1@gmail.com>
2022-03-26 12:56:17 -0500

    Bump plotly.js to 2.11.1 (#2096)

--- R/plotly.R
+++ R/plotly.R
@@ -479,7 +479,7 @@ typedArrayPolyfill <- function() {
 plotlyMainBundle <- function() {
   htmltools::htmlDependency(
     name = "plotly-main", 
-    version = "2.5.1",
+    version = "2.11.1",
     package = "plotly",
     src = dependency_dir("plotlyjs"),
     script = "plotly-latest.min.js",
