200b0e2e3aa54f51288dfa6558012f544e9e6991
Carson <cpsievert1@gmail.com>
2022-11-02 11:54:44 -0500

    Skip warnPartialMatchArgs test on R 3.5

--- tests/testthat/test-ggplot-warnings.R
+++ tests/testthat/test-ggplot-warnings.R
@@ -1,4 +1,6 @@
 test_that("ggplotly does not issue partial-argument-match warning", {
+  skip_if_not(getRversion() >= "3.6")
+  
   opts <- options(warnPartialMatchArgs = TRUE)
   on.exit(options(opts), add = TRUE)
   p <- ggplot(data.frame())
