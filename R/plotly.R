d6c36ce036496374d8d398c445d262cf41a6e829
jrwishart <justin.wishart@displayr.com>
2023-08-04 20:01:40 +1000

    DS-4923 Only check QTable class (#21)

--- R/plotly.R
+++ R/plotly.R
@@ -142,7 +142,7 @@ plot_ly <- function(data = data.frame(), ..., type = NULL, name,
   }
 
   if (is.data.frame(data) && nrow(data) > 0L) {
-    qtables <- vapply(data, inherits, logical(1L), c("qTable", "QTable"))
+    qtables <- vapply(data, inherits, logical(1L), "QTable")
     if (any(qtables))
       data[qtables] <- lapply(data[qtables], as.vector)
   }
