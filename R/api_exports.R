44ba0c0d9474c6e182443b6550d5426c3a83594d
Salim B <git@salim.space>
2024-09-04 23:32:09 +0200

    fix: remove duplicated roxygen2 tags

--- R/api_exports.R
+++ R/api_exports.R
@@ -107,11 +107,6 @@
 #' api("folders", "POST", list(path = "/starts/at/root/and/ends/here"))
 #' 
 #' }
-#' 
-
-
-#' @rdname api
-#' @export
 api_create <- function(x = last_plot(), filename = NULL, 
                        fileopt = c("overwrite", "new"),
                        sharing = c("public", "private", "secret"), ...) {
