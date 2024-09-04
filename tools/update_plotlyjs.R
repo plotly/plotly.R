93ef777a672852a2aa48c9f7ed5c4c2f995713b5
Salim B <git@salim.space>
2024-09-04 18:36:13 +0200

    refactor: revert dependency on R >= 3.5

--- tools/update_plotlyjs.R
+++ tools/update_plotlyjs.R
@@ -80,7 +80,8 @@ withr::with_dir(tmpdir, {
           internal = TRUE,
           overwrite = TRUE,
           compress = "xz",
-          version = 3L
+          # TODO: use `version = 3L` once we depend on R (>= 3.5.0)
+          version = 2L
         )
       )
       
