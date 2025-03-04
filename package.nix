8ff12e23b92858207798bf811ad481a6e4232103
Bruce Tan <bruce.tan@displayr.com>
2025-03-04 15:18:26 +1300

    update format

--- package.nix
+++ package.nix
@@ -5,6 +5,28 @@ pkgs.rPackages.buildRPackage {
   version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
   src = ./.;
   description = "Create interactive web graphics from 'ggplot2' graphs and/or a custom interface to the (MIT-licensed) JavaScript library 'plotly.js' inspired by the grammar of graphics.";
-  propagatedBuildInputs = with pkgs.rPackages; [ tools scales httr jsonlite magrittr digest viridisLite base64enc htmltools htmlwidgets tidyr RColorBrewer dplyr vctrs tibble lazyeval rlang crosstalk purrr data_table promises ];
+  propagatedBuildInputs = with pkgs.rPackages; [ 
+    ggplot2
+    scales 
+    httr 
+    jsonlite 
+    magrittr 
+    digest 
+    viridisLite 
+    base64enc 
+    htmltools 
+    htmlwidgets 
+    tidyr 
+    RColorBrewer 
+    dplyr 
+    vctrs 
+    tibble 
+    lazyeval 
+    rlang 
+    crosstalk 
+    purrr 
+    data_table 
+    promises 
+  ];
 
 }
